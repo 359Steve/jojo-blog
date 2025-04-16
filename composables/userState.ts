d4bd78b15b53e85423f0f5aed932f478589f96fd
huhaiaaa <10217296+hu-hai-aaa@user.noreply.gitee.com>
2025-04-16 09:37:04 +0800

    用户状态管理

---
+++ composables/userState.ts
@@ -0,0 +1,23 @@
+import { defineStore } from 'pinia'
+
+export const useUserState = defineStore('userState', () => {
+    const token = ref<string>('')
+
+    // 获取token
+    const getToken = () => {
+        return token.value
+    }
+
+    // 设置token
+    const setToken = (value: string) => {
+        token.value = value
+    }
+
+    return{
+        token,
+        getToken,
+        setToken
+    }
+}, {
+    persist: true
+})
