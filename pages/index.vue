61155165a968937fc0c03971d4197601c0b6dcdc
huhaiaaa <10217296+hu-hai-aaa@user.noreply.gitee.com>
2025-04-21 19:54:42 +0800

    has的用法

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,49 @@
 <script lang='ts' setup>
-
+const check = reactive([
+    { title: 'Google Pay', value: false },
+    { title: 'Apple Pay', value: false },
+    { title: 'PayPal', value: false },
+])
 </script>
 
 <template>
-    <div class="home group w-full bg-[#DBDBDB] flex justify-center items-center hover:cursor-pointer">
-        <span class="before:content-['🔥'] peer-hover:text-[pink]">Index</span>
-        <div class="peer">兄弟元素</div>
+    <div class="mt-4 space-y-2">
+        <label 
+            v-for="item in check" 
+            :key="item.title" 
+            class="w-full flex justify-between
+                items-center
+                rounded-lg p-4
+                hover:bg-gray-100 
+                has-[input:checked]:bg-indigo-50 
+                has-[input:checked]:text-indigo-800 
+                has-[input:checked]:ring-indigo-200 
+                has-[input:checked]:ring-1
+                dark:hover:bg-white/5 
+                dark:has-[input:checked]:bg-indigo-950 
+                dark:has-[input:checked]:text-indigo-200 
+                dark:has-[input:checked]:ring-indigo-900"
+        >
+            <span>{{ item.title }}</span>
+            <input id="apple" 
+                    type="radio"
+                    class="box-content 
+                        h-1.5 w-1.5 
+                        appearance-none 
+                        rounded-full 
+                        border-[5px] 
+                        border-white 
+                        bg-white bg-clip-padding 
+                        ring-1 ring-gray-950/20 
+                        outline-none 
+                        checked:border-indigo-500 
+                        checked:ring-indigo-500"
+                    name="payment_method" 
+                    :checked="item.value" 
+                    value="google"
+            >
+        </label>
     </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
