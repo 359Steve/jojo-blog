e6426d5009517731e2f0b2a9a9e2142eb45f2362
huhaiaaa <10217296+hu-hai-aaa@user.noreply.gitee.com>
2025-04-28 20:37:09 +0800

    引入md组件并调用封装的get请求读取md文件内容

--- pages/diary/index.vue
+++ pages/diary/index.vue
@@ -1,9 +1,17 @@
 <script lang='ts' setup>
-
+const mdText = ref<string>('')
+const res = await useGet<any, { content: string }> ('/md')
+if (res.code === 200) {
+    mdText.value = res.data.content
+}
 </script>
 
 <template>
-    <div>diary的子页面</div>
+    <div class="w-full">
+        <ClientOnly>
+            <MdEditor v-model="mdText" theme="light" />
+        </ClientOnly>
+    </div>
 </template>
 
 <style lang='scss' scoped>
