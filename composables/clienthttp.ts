0b437f94f8915b80e352b7f1bb50e74a772ca937
huhaiaaa <10217296+hu-hai-aaa@user.noreply.gitee.com>
2025-04-29 16:45:18 +0800

    封装客户端请求

---
+++ composables/clienthttp.ts
@@ -0,0 +1,120 @@
+type Method = 'get' | 'post' | 'put' | 'delete'
+// 自定义 Options 类型
+type Options<T> = {
+    baseURL?: string
+    method?: Method
+    body?: T
+    query?: T
+    params?: T
+    headers?: Record<string, string>
+    [key: string]: any
+}
+// 响应基本信息类型
+type BaseResponse<T> = {
+    code: number
+    msg: string
+    data: T
+    [key: string]: any
+}
+
+export const fetchApiCore = <Rq = any, Rp = any>(url: string, option: Options<Rq | any>) => {
+    // 获取全局变量
+    const appConfig = useAppConfig()
+    // 获取nuxtApp实例
+    const nuxtApp = useNuxtApp()
+    // 获取token
+    const token: string = useUserState().getToken() || ''
+
+    return $fetch<BaseResponse<Rp>>(url, {
+        baseURL: appConfig.baseUrl,
+        
+        // 设置请求拦截
+        onRequest({ options }) {
+            options.headers = {
+                Authorization: `Bearer ${token}`,
+                ...options.headers
+            } as Headers & { Authorization?: string }
+        },
+
+        // 响应拦截
+        onResponse({ response }) {
+            if (response.ok) {
+                const data = response._data as BaseResponse<Rp>
+                if (data.code !== 200) {
+                    if (import.meta.client) {
+                        // 直接提示错误信息
+                        ElMessage({
+                            type: 'error',
+                            message: data.msg
+                        })
+                    } else {
+                        // 跳转错误页面
+                        nuxtApp.runWithContext(() => {
+                            navigateTo({
+                                path: '/pageError',
+                                query: {
+                                    code: data.code,
+                                    msg: data.msg
+                                }
+                            })
+                        })
+                    }
+                }
+            }
+        },
+
+        // 响应失败
+        onResponseError({ response }) {
+            const data = response._data as BaseResponse<Rp>
+            // 如果是客户端直接提示错误信息
+            if (import.meta.client) {
+                ElMessage({
+                    type: 'error',
+                    message: data.msg
+                })
+            } else {
+                // 跳转错误页面
+                nuxtApp.runWithContext(() => {
+                    navigateTo({
+                        path: '/pageError',
+                        query: {
+                            code: data.code,
+                            msg: data.msg
+                        }
+                    })
+                })
+            }
+        },
+
+        method: option?.method as any,
+        ...option,
+    })
+}
+
+// GET请求封装
+export const fetchUseGet = <Rq = any, Rp = any>(url: string, option: Options<Rq> = {}): Promise<BaseResponse<Rp>> => {
+    return new Promise((resolve, reject) => {
+        fetchApiCore<Rq, Rp>(url, {
+            method: 'get',
+            ...option
+        }).then(res => {
+            resolve(res as BaseResponse<Rp>)
+        }).catch(err => {
+            reject(err)
+        })
+    })
+}
+
+// POST 封装
+export const fetchPostApi = <Rq = any, Rp = any>(url: string, option?: Options<Rq>): Promise<BaseResponse<Rp>> => {
+    return new Promise((resolve, reject) => {
+        fetchApiCore<Rq, Rp>(url, {
+            method: 'post',
+            ...option
+        }).then(res => {
+            resolve(res as BaseResponse<Rp>)
+        }).catch(err => {
+            reject(err)
+        })
+    })
+}
