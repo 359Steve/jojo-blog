2b93d00926dedca6c4d79365b61551be1157c6b9
huhaiaaa <10217296+hu-hai-aaa@user.noreply.gitee.com>
2025-04-29 14:21:26 +0800

    定义主题布局框架

---
+++ layouts/mainbox.vue
@@ -0,0 +1,13 @@
+<script lang='ts' setup>
+
+</script>
+
+<template>
+    <div class="w-full sm:max-w-xl md:max-w-2xl lg:max-w-4xl m-auto p-4 box-border">
+        <slot name="childPage"></slot>
+    </div>
+</template>
+
+<style lang='scss' scoped>
+
+</style>
