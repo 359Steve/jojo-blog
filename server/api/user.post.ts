c67521e3fa28866eee78b1c53782c2f5249ae71d
hu-hai-aaa <2075313210@qq.com>
2025-06-09 18:37:31 +0800

    修改接口为controller风格

--- server/api/user.post.ts
+++ server/api/user.post.ts
@@ -1,29 +1,8 @@
-import type { PrismaClient, user_info } from '@prisma/client';
-import { getDb } from '~/server/utils/db';
+import { UserController } from '../controllers/UserController';
+import { container } from '../core/container';
 
 export default defineEventHandler(async event => {
-	// 获取body
-	const { user_name, password, avatar_url, pet_name } = await readBody<user_info>(event);
-	const db: PrismaClient = getDb();
-	const res = await db.user_info.create({
-		data: {
-			user_name,
-			password,
-			avatar_url,
-			pet_name
-		}
-	});
-	const result_ok = {
-		code: 200,
-		msg: 'ok',
-		data: res
-	};
+	const userController = container.get(UserController);
 
-	const result_error = {
-		code: 500,
-		msg: '注册失败',
-		data: null
-	};
-
-	return res ? result_ok : result_error;
+	return await userController.createUser(event);
 });
