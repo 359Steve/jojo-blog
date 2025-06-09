44040f8d6fb3d21cfa667eaf3f28ec3bc3508c84
hu-hai-aaa <2075313210@qq.com>
2025-06-09 18:33:38 +0800

    关闭自动排序

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,8 @@ export default defineConfig(
 			// 禁用 prettier 插件的规则（如 soybeans 内部引用了）
 			'prettier/prettier': 'off',
 			'no-underscore-dangle': 'off',
+			// 关闭自动排序
+			'import/order': 'off',
 		},
 	},
 );
