0a1a1d6765a6b5d1de5cbef6ff76b64ca6eef448
hu-hai-aaa <2075313210@qq.com>
2025-06-09 18:37:21 +0800

    修改接口为controller风格

--- server/api/user-find.get.ts
+++ server/api/user-find.get.ts
@@ -1,36 +1,11 @@
-import { readFile } from 'node:fs/promises';
-import { join } from 'node:path';
-import process from 'node:process';
+import { container } from '../core/container';
+import { UserService } from '../services/UserService';
 
 export default defineEventHandler(async event => {
+	const userInfoService = container.get(UserService);
 	// 获取query参数
 	const { id } = getQuery<{ id: number }>(event);
-	const db = getDb();
 
 	// 查询用户信息
-	const result = await db.user_info.findUnique({
-		where: {
-			id: Number(id)
-		}
-	});
-
-	if (result) {
-		const { describe } = result;
-		// 读取文件
-		const filePath = join(process.cwd(), 'public/file', `${describe}.txt`);
-		const content = await readFile(filePath, 'utf-8');
-		return {
-			code: 200,
-			msg: 'ok',
-			data: {
-				content
-			}
-		};
-	}
-
-	return {
-		code: 500,
-		msg: '查询失败',
-		data: null
-	};
+	return await userInfoService.findUser(id);
 });
