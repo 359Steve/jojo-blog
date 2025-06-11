018e6f4271284fd35357d8d9b9b591333f486f87
hu-hai-aaa <2075313210@qq.com>
2025-06-11 18:42:43 +0800

    删除controller注解

--- server/core/decorators.ts
+++
@@ -1,8 +0,0 @@
-// core/decorators.ts
-import { container } from './container';
-
-export function controller<T extends { new(...args: any[]): object }>(target: T) {
-	if (!container.isBound(target)) {
-		container.bind(target).toSelf();
-	}
-}
