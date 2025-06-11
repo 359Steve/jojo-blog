e068b58ab2108dd04d790e65d4353f0e391e8e74
hu-hai-aaa <2075313210@qq.com>
2025-06-11 18:40:00 +0800

    创建登录接口

---
+++ server/api/user-login.post.ts
@@ -0,0 +1,11 @@
+import { container } from '../core/container';
+import type { CreateUserDto } from '../dto/CreateUserDto';
+import { UserService } from '../services/UserService';
+
+export default defineEventHandler(async event => {
+	const userService = container.get(UserService);
+	// 读取body参数
+	const body = await readBody<CreateUserDto>(event);
+
+	return await userService.loginUser(body);
+});
