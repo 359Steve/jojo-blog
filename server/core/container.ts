f058eadaee419d924999684dee683a53a6f14e6e
hu-hai-aaa <2075313210@qq.com>
2025-06-11 18:42:26 +0800

    删除controller的contaner的引入

--- server/core/container.ts
+++ server/core/container.ts
@@ -3,7 +3,7 @@ import { UserRepository } from '../repositories/UserRepository';
 import { UserService } from '../services/UserService';
 
 const container = new Container();
-container.bind(UserService).toSelf();
 container.bind(UserRepository).toSelf();
+container.bind(UserService).toSelf();
 
 export { container };
