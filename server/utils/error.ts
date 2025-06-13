38db9416a446711dbbcd767f3be91704e7f4a6e8
hu-hai-aaa <2075313210@qq.com>
2025-06-13 17:46:25 +0800

    封装token验证失败方法

---
+++ server/utils/error.ts
@@ -0,0 +1,14 @@
+import type { H3Event } from 'h3';
+
+export const sendErrorWithMessage = (event: H3Event, statusCode: number, message: string) => {
+	sendError(
+		event,
+		createError({
+			statusCode,
+			statusMessage: 'Error',
+			data: {
+				message
+			}
+		})
+	);
+};
