d7639e7d4ea0279976ee6dbe788a233a23e8e41e
hu-hai-aaa <2075313210@qq.com>
2025-06-13 17:41:10 +0800

    浅拷贝错误信息

--- composables/http.ts
+++ composables/http.ts
@@ -59,6 +59,7 @@ export const apiCore = <Rq = any, Rp = any>(url: string, option: Options<Rq | an
 			if (!response.ok) return;
 			const data = response._data as BaseResponse<Rp>;
 			if (data.code !== 200) {
+				const error = data as BaseResponse<{ message: string }>;
 				if (import.meta.client) {
 					// 直接提示错误信息
 					ElMessage({
