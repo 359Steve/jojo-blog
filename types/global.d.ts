159868b7caabc4e5ba4188893316872e38333148
hu-hai-aaa <2075313210@qq.com>
2025-06-13 17:46:46 +0800

    创建全局类型文件并自定义类型

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,1 +1,18 @@
 declare module 'esbuild-plugin-decorators';
+
+declare global {
+	interface JoJoResponse<T> {
+		code: number;
+		msg: string;
+		data: T | null;
+	}
+
+	interface TokenCookie {
+		userState: {
+			token: string;
+		};
+		token: string;
+	}
+}
+
+export { };
