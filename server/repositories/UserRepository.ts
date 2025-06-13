a104da860963057b5f2a04dee013126db1c92ad2
hu-hai-aaa <2075313210@qq.com>
2025-06-13 17:45:40 +0800

    格式化接口返回数据

--- server/repositories/UserRepository.ts
+++ server/repositories/UserRepository.ts
@@ -1,4 +1,7 @@
 import { PrismaClient } from '@prisma/client';
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import process from 'node:process';
 import type { CreateUserDto } from '../dto/CreateUserDto';
 
 export class UserRepository {
@@ -8,29 +11,82 @@ export class UserRepository {
 		this.prisma = new PrismaClient();
 	}
 
-	async createUser(dto: CreateUserDto) {
-		return this.prisma.user_info.create({
+	async createUser<T>(dto: CreateUserDto): Promise<JoJoResponse<T>> {
+		const res = await this.prisma.user_info.create({
 			data: {
 				...dto
 			}
 		});
+
+		const result_ok = {
+			code: 200,
+			msg: 'ok',
+			data: res as T
+		};
+
+		const result_error = {
+			code: 500,
+			msg: '注册失败',
+			data: null
+		};
+
+		return res ? result_ok : result_error;
 	}
 
-	async loginUser(body: CreateUserDto) {
+	async loginUser<T>(body: CreateUserDto): Promise<JoJoResponse<T>> {
 		const { user_name, password } = body;
-		return this.prisma.user_info.findFirst({
+		const res = await this.prisma.user_info.findFirst({
 			where: {
 				user_name,
 				password
 			}
 		});
+
+		if (res) {
+			// 生成token
+			const accessToken = signToken(res);
+
+			return {
+				code: 200,
+				msg: '登录成功',
+				data: {
+					accessToken
+				} as T
+			};
+		}
+
+		return {
+			code: 500,
+			msg: '登录失败',
+			data: null
+		};
 	}
 
-	async findUser(id: number) {
-		return this.prisma.user_info.findUnique({
+	async findUser<T>(id: number): Promise<JoJoResponse<T>> {
+		const res = await this.prisma.user_info.findUnique({
 			where: {
 				id: Number(id)
 			}
 		});
+
+		if (res) {
+			// 读取文件
+			const { describe } = res;
+			const filePath = join(process.cwd(), 'public/file', `${describe}.txt`);
+			const content = await readFile(filePath, 'utf-8');
+
+			return {
+				code: 200,
+				msg: 'ok',
+				data: {
+					content
+				} as T
+			};
+		}
+		return {
+			code: 500,
+			msg: '查询失败',
+			data: null
+		};
 	}
 }
