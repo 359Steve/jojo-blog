544ff8af01ea15e4e91923caff99123eee8b0ae9
hu-hai-aaa <2075313210@qq.com>
2025-06-13 17:44:32 +0800

    穿件用户查询接口

---
+++ server/api/user-query.get.ts
@@ -0,0 +1,22 @@
+import { container } from '../core/container';
+import { UserService } from '../services/UserService';
+import { sendErrorWithMessage } from '../utils/error';
+
+// 返回值类型
+interface FindResponse {
+	content: string;
+}
+
+export default defineEventHandler(async event => {
+	const userService = container.get(UserService);
+	// 读取qeruy参数
+	const { id } = getQuery<{ id: number }>(event);
+
+	try {
+		// 查询用户信息
+		return await userService.findUser<FindResponse>(id);
+	} catch {
+		sendErrorWithMessage(event, 500, '查询失败！');
+		return null;
+	}
+});
