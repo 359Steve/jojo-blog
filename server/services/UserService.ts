1bf33e8e0e4e470d87be5b525ab08bc4e902c956
hu-hai-aaa <2075313210@qq.com>
2025-06-13 17:46:06 +0800

    格式化service层调用实现类方式

--- server/services/UserService.ts
+++ server/services/UserService.ts
@@ -1,6 +1,3 @@
-import { readFile } from 'node:fs/promises';
-import { join } from 'node:path';
-import process from 'node:process';
 import { container } from '../core/container';
 import type { CreateUserDto } from '../dto/CreateUserDto';
 import { UserRepository } from '../repositories/UserRepository';
@@ -14,70 +11,17 @@ export class UserService {
 	}
 
 	// 创建用户
-	async createUser(dto: CreateUserDto) {
-		const res = await this.userRepo.createUser(dto);
-
-		const result_ok = {
-			code: 200,
-			msg: 'ok',
-			data: res
-		};
-
-		const result_error = {
-			code: 500,
-			msg: '注册失败',
-			data: null
-		};
-
-		return res ? result_ok : result_error;
+	async createUser<T>(dto: CreateUserDto): Promise<JoJoResponse<T>> {
+		return await this.userRepo.createUser<T>(dto);
 	}
 
 	// 登录
-	async loginUser(body: CreateUserDto) {
-		const res = await this.userRepo.loginUser(body);
-
-		if (res) {
-			// 生成token
-			const accessToken = signToken(res);
-
-			return {
-				code: 200,
-				msg: '登录成功',
-				data: {
-					accessToken
-				}
-			};
-		}
-
-		return {
-			code: 500,
-			msg: '登录失败',
-			data: null
-		};
+	async loginUser<T>(body: CreateUserDto): Promise<JoJoResponse<T>> {
+		return await this.userRepo.loginUser<T>(body);
 	}
 
 	// 查询用户
-	async findUser(id: number) {
-		const res = await this.userRepo.findUser(id);
-
-		if (res) {
-			// 读取文件
-			const { describe } = res;
-			const filePath = join(process.cwd(), 'public/file', `${describe}.txt`);
-			const content = await readFile(filePath, 'utf-8');
-
-			return {
-				code: 200,
-				msg: 'ok',
-				data: {
-					content
-				}
-			};
-		}
-		return {
-			code: 500,
-			msg: '查询失败',
-			data: null
-		};
+	async findUser<T>(id: number): Promise<JoJoResponse<T>> {
+		return await this.userRepo.findUser<T>(id);
 	}
 }
