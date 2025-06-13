2f606c5f8b45937651193c5392966cbffd5b73ff
hu-hai-aaa <2075313210@qq.com>
2025-06-13 16:45:52 +0800

    设置timelineDate默认值为[]

--- components/index/BasicTime.vue
+++ components/index/BasicTime.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import type { Timeline } from '~/types/com-types';
 
-const { timelineData } = defineProps<{
+const { timelineData = [] } = defineProps<{
 	timelineData: Timeline[];
 }>();
 
@@ -13,35 +13,22 @@ defineEmits<{
 <template>
 	<div class="h-full w-full">
 		<div v-for="(item, index) in timelineData" :key="item.id" class="grid grid-cols-2">
-			<div :class="[`relative h-full w-full`, (index + 1) % 2 === 0 ? 'col-start-2 pl-4' : 'col-start-1 pr-4']">
-				<div
-					:class="[
-						`absolute size-3 rounded-[50%] bg-[#E4E7EC]`,
-						(index + 1) % 2 === 0 ? 'left-[calc(-0.75rem/2)]' : 'right-[calc(-0.75rem/2)]',
-					]"
-				></div>
-				<div
-					:class="[`absolute h-full w-[4px] bg-[#E4E7EC]`, (index + 1) % 2 === 0 ? 'left-[-2px]' : 'right-[-2px]']"
-				></div>
+			<div class="relative h-full w-full"
+				:class="[(index + 1) % 2 === 0 ? 'col-start-2 pl-4' : 'col-start-1 pr-4']">
+				<div class="absolute size-3 rounded-[50%] bg-[#E4E7EC]"
+					:class="[(index + 1) % 2 === 0 ? 'left-[calc(-0.75rem/2)]' : 'right-[calc(-0.75rem/2)]']"></div>
+				<div class="absolute h-full w-[4px] bg-[#E4E7EC]"
+					:class="[(index + 1) % 2 === 0 ? 'left-[-2px]' : 'right-[-2px]']"></div>
 				<div class="h-full w-full">
-					<div
-						:class="[
-							`flex w-full items-center text-xs text-gray-400`,
-							(index + 1) % 2 === 0 ? 'justify-start' : 'justify-end',
-						]"
-					>
+					<div class="flex w-full items-center text-xs text-gray-400"
+						:class="[(index + 1) % 2 === 0 ? 'justify-start' : 'justify-end']">
 						<span>{{ item.timestamp }}</span>
 					</div>
 					<AnimationRevealOnScroll
-						:animation-class="(index + 1) % 2 === 0 ? 'animate__fadeInRight' : 'animate__fadeInLeft'"
-					>
-						<div
-							@click="$emit('detailRecord', item)"
-							:class="[
-								`mt-2 flex cursor-pointer items-center rounded-base p-2 shadow-lg backdrop-blur transition-all hover:bg-gray-50 dark:shadow-[0_4px_20px_rgba(255,255,255,0.05)] dark:hover:bg-gray-100/10`,
-								(index + 1) % 2 === 0 ? 'text-left' : 'text-right',
-							]"
-						>
+						:animation-class="(index + 1) % 2 === 0 ? 'animate__fadeInRight' : 'animate__fadeInLeft'">
+						<div class="mt-2 flex cursor-pointer items-center rounded-base p-2 shadow-lg backdrop-blur transition-all hover:bg-gray-50 dark:shadow-[0_4px_20px_rgba(255,255,255,0.05)] dark:hover:bg-gray-100/10"
+							:class="[(index + 1) % 2 === 0 ? 'text-left' : 'text-right']"
+							@click="$emit('detailRecord', item)">
 							<img :src="item.url" alt="timestamp" class="mr-2 size-8" />
 							<div class="h-full w-[calc(100%-2rem)] break-normal text-center">
 								<span>{{ item.description }}</span>
