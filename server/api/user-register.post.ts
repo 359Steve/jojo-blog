540149606d2fa3d70c5f3518c8777b82dbe25626
hu-hai-aaa <2075313210@qq.com>
2025-06-13 17:44:51 +0800

    创建用户注册接口

---
+++ server/api/user-register.post.ts
@@ -0,0 +1,17 @@
+import { container } from '../core/container';
+import type { CreateUserDto } from '../dto/CreateUserDto';
+import { UserService } from '../services/UserService';
+import { sendErrorWithMessage } from '../utils/error';
+
+export default defineEventHandler(async event => {
+	const userService = container.get(UserService);
+	// 读取body参数
+	const body = await readBody<CreateUserDto>(event);
+
+	try {
+		return await userService.createUser<CreateUserDto>(body);
+	} catch {
+		sendErrorWithMessage(event, 500, '注册失败！');
+		return null;
+	}
+});
