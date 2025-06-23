ccd378ef0dd60328eb4fae8c4d31da530d042ffd
hu-hai-aaa <2075313210@qq.com>
2025-06-23 09:45:43 +0800

    图片修改成icon

--- components/footer/FooterBox.vue
+++ components/footer/FooterBox.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import BiliIcon from './utils/bilibili';
 const queryData = async () => {
 	const res = await fetchPostApi<{ user_name: string; password: string }, { accessToken: string }>('/user-login', {
 		body: {
@@ -17,14 +18,14 @@ const queryData = async () => {
 	<div class="mt-8 grid w-full grid-cols-1 gap-2 py-2 sm:grid-cols-2">
 		<div class="flex items-center justify-center sm:hidden">
 			<a href="https://space.bilibili.com/457627448?spm_id_from=333.1007.0.0" target="_blank"
-				rel="noopener noreferrer">
-				<img class="mr-4 h-4 cursor-pointer" src="~/assets/image/bilibili.png" alt="我的b站" />
+				rel="noopener noreferrer" class="flex items-center gap-2">
+				<BiliIcon icon-class="text-base"></BiliIcon>
 			</a>
 		</div>
 		<div class="col-span-1 flex items-center justify-center sm:col-span-2">
 			<a href="https://space.bilibili.com/457627448?spm_id_from=333.1007.0.0" target="_blank"
-				rel="noopener noreferrer">
-				<img class="mr-4 hidden h-4 cursor-pointer sm:block" src="~/assets/image/bilibili.png" alt="我的b站" />
+				rel="noopener noreferrer" class="mr-4 flex items-center gap-2">
+				<BiliIcon icon-class="hidden sm:block"></BiliIcon>
 			</a>
 			<span class="text-xs" @click="queryData">Copyright © 2023 Josef Joestar</span>
 		</div>
