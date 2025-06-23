6ea79880e97b2429c88ebaf8d0bfb92fc91ff35d
hu-hai-aaa <2075313210@qq.com>
2025-06-23 09:47:16 +0800

    添加离线图标依赖

--- plugins/iconify.ts
+++ plugins/iconify.ts
@@ -1,5 +1,7 @@
-import { Icon } from '@iconify/vue';
+import ri from '@iconify-json/ri/icons.json';
+import { Icon, addCollection } from '@iconify/vue';
 
 export default defineNuxtPlugin(nuxtApp => {
 	nuxtApp.vueApp.component('Icon', Icon);
+	addCollection(ri);
 });
