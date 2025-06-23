f64f5adae1e13caef2c9b425348012ec1cff7cfb
hu-hai-aaa <2075313210@qq.com>
2025-06-23 09:47:53 +0800

    创建全局布局控制框架

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+const route = useRoute();
+
+// 获取当前路由路径
+const isAdmin = computed(() => route.path.startsWith('/admin'));
+</script>
+
+<template>
+	<!-- 后台系统 -->
+	<NuxtLayout v-if="isAdmin" name="admin">
+		<template #page>
+			<slot />
+		</template>
+	</NuxtLayout>
+
+	<!-- 前台系统：jojomain 嵌套 mainbox -->
+	<NuxtLayout v-else name="jojomain">
+		<template #page>
+			<NuxtLayout v-if="!isAdmin" name="mainbox">
+				<template #childPage>
+					<StarportCarrier>
+						<slot />
+					</StarportCarrier>
+				</template>
+			</NuxtLayout>
+		</template>
+	</NuxtLayout>
+</template>
+
+<style lang="scss" scoped></style>
