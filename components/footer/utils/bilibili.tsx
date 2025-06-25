bf5537783034262159c1a4daea7ce7d615f25ae0
hu-hai-aaa <2075313210@qq.com>
2025-06-25 15:37:57 +0800

    格式化代码

--- components/footer/utils/bilibili.tsx
+++ components/footer/utils/bilibili.tsx
@@ -4,22 +4,10 @@ import { Fragment, defineComponent, h } from 'vue';
 export default defineComponent({
 	name: 'BiliIcon',
 	props: {
-		iconName: {
-			type: String,
-			default: 'ri:bilibili-line'
-		},
-		textName: {
-			type: String,
-			default: 'BiliBili'
-		},
-		iconClass: {
-			type: String,
-			default: ''
-		},
-		textClass: {
-			type: String,
-			default: 'text-xs'
-		}
+		iconName: { type: String, default: 'ri:bilibili-line' },
+		textName: { type: String, default: 'BiliBili' },
+		iconClass: { type: String, default: '' },
+		textClass: { type: String, default: 'text-xs' }
 	},
 	render() {
 		return h(Fragment, null, [
