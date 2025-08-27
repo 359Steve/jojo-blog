92146f0aeb8908f6ead02174ccad2622066486d9
hu-hai-aaa <2075313210@qq.com>
2025-08-27 18:42:00 +0800

    添加token验证

--- admin/middleware/admin.ts
+++ admin/middleware/admin.ts
@@ -1,17 +1,26 @@
-export default defineNuxtRouteMiddleware(to => {
+import { StatusCode } from "~/types/com-types";
+
+export default defineNuxtRouteMiddleware(async to => {
 	// 路由白名单
 	const whiteRoutes = ['/admin/login'];
 	const path = to.path;
 	const cookies = useUserState().getToken();
 
 	if (!whiteRoutes.includes(path)) {
-		if (!cookies) return navigateTo('/admin/login');
-		try {
-			// 验证token
-			const payload = verifyToken(cookies || '');
-			if (!payload) return navigateTo('/admin/login');
-		} catch {
+		if (!cookies) {
 			return navigateTo('/admin/login');
+		} else {
+			// 验证token
+			try {
+				const res = await postApi<{ token: string }, any>('/verify-token', {
+					body: {
+						token: cookies
+					}
+				})
+				return res.code === StatusCode.SUCCESS ? true : navigateTo('/admin/login');
+			} catch (error) {
+				return navigateTo('/admin/login');
+			}
 		}
 	}
 
