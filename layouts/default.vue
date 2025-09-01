bf32f1caa8d5ba5890d73558255cd942101f8f95
hu-hai-aaa <2075313210@qq.com>
2025-09-01 18:15:15 +0800

    动态判断布局

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,26 +3,13 @@ const route = useRoute();
 
 // 获取当前路由路径
 const isAdmin = computed(() => route.path.startsWith('/admin'));
+const layout = computed(() => isAdmin.value ? 'admin' : 'jojomain')
 </script>
 
 <template>
-	<!-- 后台系统 -->
-	<NuxtLayout v-if="isAdmin" name="admin">
+	<NuxtLayout :name="layout">
 		<template #page>
 			<slot />
-		</template>
-	</NuxtLayout>
-
-	<!-- 前台系统：jojomain 嵌套 mainbox -->
-	<NuxtLayout v-else name="jojomain">
-		<template #page>
-			<NuxtLayout v-if="!isAdmin" name="mainbox">
-				<template #childPage>
-					<StarportCarrier>
-						<slot />
-					</StarportCarrier>
-				</template>
-			</NuxtLayout>
 		</template>
 	</NuxtLayout>
 </template>
