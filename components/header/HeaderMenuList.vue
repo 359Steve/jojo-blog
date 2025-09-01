64c74ac6d5f9957574f5e9869ffdb2bc3da7dc9e
hu-hai-aaa <2075313210@qq.com>
2025-09-01 18:14:54 +0800

    修改导航拦切换逻辑

--- components/header/HeaderMenuList.vue
+++ components/header/HeaderMenuList.vue
@@ -1,34 +1,27 @@
 <script lang="ts" setup>
 const { menuList, drawer, menuId } = storeToRefs(useJojoHeader());
 
-const toPath = (item: (typeof menuList.value)[number]) => {
-	menuId.value = item.id;
+const toPath = async (item: (typeof menuList.value)[number]) => {
 	drawer.value = false;
-	navigateTo(item.path);
+	await navigateTo(item.path);
+	menuId.value = item.id;
 };
 </script>
 
 <template>
 	<div class="w-full">
 		<ul class="w-full items-center justify-center sm:flex sm:gap-x-12">
-			<li
-				v-for="item in menuList"
-				:key="item.id"
-				:class="[
-					`mb-4 rounded-base px-4 leading-[3rem] shadow-sm hover:cursor-pointer sm:mb-0 sm:h-full sm:rounded-none sm:px-0 sm:shadow-none`,
+			<li v-for="item in menuList" :key="item.id" :class="[
+				`mb-4 rounded-base px-4 leading-[3rem] shadow-sm hover:cursor-pointer sm:mb-0 sm:h-full sm:rounded-none sm:px-0 sm:shadow-none`,
+				item.id === menuId
+					? 'bg-black shadow-none dark:bg-[white] sm:bg-inherit sm:dark:bg-inherit'
+					: 'hover:bg-[#DBDBDB] hover:text-black sm:hover:bg-transparent sm:dark:text-white',
+			]" @click="toPath(item)">
+				<span :class="[
 					menuId === item.id
-						? 'bg-black shadow-none dark:bg-[white] sm:bg-inherit sm:dark:bg-inherit'
-						: 'hover:bg-[#DBDBDB] hover:text-black sm:hover:bg-transparent sm:dark:text-white',
-				]"
-				@click="toPath(item)"
-			>
-				<span
-					:class="[
-						menuId === item.id
-							? 'font-bold text-white dark:text-black sm:text-black sm:dark:text-white'
-							: 'text-gray-500',
-					]"
-				>
+						? 'font-bold text-white dark:text-black sm:text-black sm:dark:text-white'
+						: 'text-gray-500',
+				]">
 					{{ item.title }}
 				</span>
 			</li>
