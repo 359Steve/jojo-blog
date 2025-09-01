b16d20a5a1c22762a30fe79dc947de75475fa359
hu-hai-aaa <2075313210@qq.com>
2025-09-01 18:14:35 +0800

    动态判断布局

--- admin/layouts/admin.vue
+++ admin/layouts/admin.vue
@@ -7,17 +7,12 @@ const route = useRoute();
 
 // 获取当前路由路径
 const isLogin = computed(() => route.path.includes('/login'));
+const layout = computed(() => isLogin.value ? 'login' : 'main')
 </script>
 
 <template>
 	<div class="relative h-fit w-full">
-		<!-- 登录布局 -->
-		<NuxtLayout v-if="isLogin" name="login">
-			<slot name="page" />
-		</NuxtLayout>
-
-		<!-- 系统布局 -->
-		<NuxtLayout v-else name="main">
+		<NuxtLayout :name="layout">
 			<slot name="page" />
 		</NuxtLayout>
 	</div>
