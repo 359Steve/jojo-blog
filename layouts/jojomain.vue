e19237d6040204f0f8f51dbab090c1890c7cbaad
hu-hai-aaa <2075313210@qq.com>
2025-09-04 18:10:24 +0800

    修改进度条颜色

--- layouts/jojomain.vue
+++ layouts/jojomain.vue
@@ -66,7 +66,7 @@ onMounted(() => {
 		<div class="h-full w-full">
 			<!-- 导航栏 -->
 			<HeaderBox :select-theme="selectTheme" @change-theme="changeTheme"></HeaderBox>
-			<NuxtLoadingIndicator />
+			<NuxtLoadingIndicator color="#A3AAB6" />
 			<NuxtLayout name="mainbox">
 				<template #childPage>
 					<StarportCarrier>
