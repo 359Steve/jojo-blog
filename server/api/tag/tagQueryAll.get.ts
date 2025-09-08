2c560e54a88c466ed46828937cabd02186daefe6
hu-hai-aaa <2075313210@qq.com>
2025-09-08 21:41:29 +0800

    创建接口

---
+++ server/api/tag/tagQueryAll.get.ts
@@ -0,0 +1,16 @@
+import { container } from "../../core/container";
+import { CreateTagDto } from "../../dto/CreateTagDto";
+import { TagService } from "../../services/TagService";
+
+export default defineEventHandler(async (event) => {
+	const tagService = container.get(TagService);
+	const query = getQuery<FindAllReq>(event)
+
+	try {
+		// 查询标签数据
+		return await tagService.findAllTag(query);
+	} catch {
+		sendErrorWithMessage(event, 500, '查询失败！');
+		return null;
+	}
+})
