c5d72d26b5caf9e490a643fe2cf4870b0bd7e022
hu-hai-aaa <2075313210@qq.com>
2025-09-10 15:50:58 +0800

    更新配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import prettier from 'eslint-plugin-prettier'
 
 export default withNuxt(
 	{
 		files: ['**/*.vue', '**/*.ts', '**/*.js', '**/*.tsx', '**/*.jsx'],
+		plugins: { prettier },
 		rules: {
 			'@typescript-eslint/no-explicit-any': 'off',
 			'@typescript-eslint/no-unused-vars': ['warn', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }], // 未使用变量黄色警告
 			'no-unused-vars': 'off',
+			'prettier/prettier': 'warn'
 		}
 	}
 )
