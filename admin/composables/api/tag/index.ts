546f2ed147a935eb0d35c9ea799f5af01f3c0d2e
hu-hai-aaa <2075313210@qq.com>
2025-09-13 18:56:36 +0800

    优化tag标签查询

--- admin/composables/api/tag/index.ts
+++ admin/composables/api/tag/index.ts
@@ -1,84 +1,94 @@
-import type { CreateTagDto } from "~/server/dto/CreateTagDto"
-import { StatusCode } from "~/types/com-types"
+import type { CreateTagDto } from '~/server/dto/CreateTagDto';
+import { StatusCode } from '~/types/com-types';
 
 // 查询全部标签
-export const queryTagAll = async (name: string, n: number = 1, s: number = 10) => {
-	const res = await fetchUseGet<FindAllReq, { records: CreateTagDto[], total: number }>('/tag/tagQueryAll', {
-		query: {
-			name,
-			pageNumber: n,
-			pageSize: s
-		}
-	})
+export const queryTagAll = async (name: string = '', n: number = 1, s: number = 10) => {
+	const isServer = import.meta.server;
+	const queryParams = {
+		name,
+		pageNumber: n,
+		pageSize: s,
+	};
+
+	let res;
+	if (isServer) {
+		res = await useGet<FindAllReq, { records: CreateTagDto[]; total: number }>('/tag/tagQueryAll', {
+			query: queryParams,
+		});
+	} else {
+		res = await fetchUseGet<FindAllReq, { records: CreateTagDto[]; total: number }>('/tag/tagQueryAll', {
+			query: queryParams,
+		});
+	}
 
 	if (res.code === StatusCode.SUCCESS) {
 		return {
 			records: res.data.records,
-			total: res.data.total
-		}
+			total: res.data.total,
+		};
 	}
 
 	return {
 		records: [],
-		total: 0
-	}
-}
+		total: 0,
+	};
+};
 
 // 创建标签
 export const createTags = async (value: CreateTagDto) => {
 	const res = await fetchPostApi<CreateTagDto, CreateTagDto>('/tag/tagCreate', {
-		body: value
-	})
+		body: value,
+	});
 
 	if (res.code === StatusCode.SUCCESS) {
 		return {
 			data: res.data,
-			msg: res.msg
-		}
+			msg: res.msg,
+		};
 	}
 
 	return {
 		data: null,
-		msg: res.msg
-	}
-}
+		msg: res.msg,
+	};
+};
 
 // 修改标签
 export const updateTags = async (value: CreateTagDto) => {
 	const res = await fetchPostApi<CreateTagDto, CreateTagDto>('/tag/tagUpdate', {
-		body: value
-	})
+		body: value,
+	});
 
 	if (res.code === StatusCode.SUCCESS) {
 		return {
 			data: res.data,
-			msg: res.msg
-		}
+			msg: res.msg,
+		};
 	}
 
 	return {
 		data: null,
-		msg: res.msg
-	}
-}
+		msg: res.msg,
+	};
+};
 
 // 删除标签
 export const deleteTags = async (id: number) => {
 	const res = await fetchDeleteApi<{ id: number }, CreateTagDto>('/tag/tagDelete', {
 		query: {
-			id
-		}
-	})
+			id,
+		},
+	});
 
 	if (res.code === StatusCode.SUCCESS) {
 		return {
 			data: res.data,
-			msg: res.msg
-		}
+			msg: res.msg,
+		};
 	}
 
 	return {
 		data: null,
-		msg: res.msg
-	}
-}
+		msg: res.msg,
+	};
+};
--- admin/pages/admin/label/index.vue
+++ admin/pages/admin/label/index.vue
@@ -4,7 +4,7 @@ import { ElFormItem, ElPopover, type FormInstance, type FormRules } from 'elemen
 
 enum TagType {
 	BLOG = '博客',
-	PERSON = '个人'
+	PERSON = '个人',
 }
 
 const ruleFormRef = templateRef('ruleFormRef');
@@ -14,18 +14,21 @@ const formData = reactive<CreateTagDto>({
 	name: '',
 	icon: '',
 	url: '',
-	type: 'BLOG'
-})
-const tableData = ref<CreateTagDto[]>([])
-const tableHeight = ref<number>(0)
-const total = ref<number>(0)
-const pageNumber = ref<number>(1)
-const searchTag = ref<string>('')
-const isEdit = ref<boolean>(false)
+	type: 'BLOG',
+});
+
+const { data } = await useAsyncData('tags', () => queryTagAll());
+const tableData = ref<CreateTagDto[]>(data.value?.records || []);
+const tableRef = templateRef('tableRef');
+const tableHeight = ref<number>(0);
+const total = ref<number>(data.value?.total || 0);
+const pageNumber = ref<number>(1);
+const searchTag = ref<string>('');
+const isEdit = ref<boolean>(false);
 
 const rulesText = (value: string): boolean => {
-	return /^[a-z0-9]+:[a-z0-9-_]+$/i.test(value)
-}
+	return /^[a-z0-9]+:[a-z0-9-_]+$/i.test(value);
+};
 
 // 创建规则
 const createTagRules = reactive<FormRules>({
@@ -42,110 +45,118 @@ const createTagRules = reactive<FormRules>({
 					callback(new Error('图标名称格式不正确'));
 				}
 			},
-			trigger: 'blur'
-		}
+			trigger: 'blur',
+		},
 	],
 	url: [{ required: true, message: '请输入链接地址', trigger: 'blur' }],
-	type: [{ required: true, message: '请选择类型', trigger: 'blur' }]
-})
+	type: [{ required: true, message: '请选择类型', trigger: 'blur' }],
+});
 
 const isIDisabled = computed(() => {
-	return !formData.name || !formData.icon || !formData.url || !formData.type
-})
+	return !formData.name || !formData.icon || !formData.url || !formData.type;
+});
 
 // 查询全部标签
 const queryTag = async (name: string, n: number) => {
-	const { records, total: t } = await queryTagAll(name, n)
+	const { records, total: t } = await queryTagAll(name, n);
 
-	tableData.value = records
-	total.value = t
-}
+	tableData.value = records;
+	total.value = t;
+};
 
 // 新增标签
 const createTag = async (formEl: FormInstance | undefined): Promise<void> => {
-	formEl?.validate(async valid => {
+	formEl?.validate(async (valid) => {
 		if (valid) {
-			const { data, msg } = await createTags(formData)
+			const { data, msg } = await createTags(formData);
 
 			ElMessage({
 				type: data ? 'success' : 'error',
-				message: msg
-			})
+				message: msg,
+			});
 
-			queryTag('', pageNumber.value)
+			queryTag('', pageNumber.value);
 		}
-	})
-}
+	});
+};
 
 // 修改标签
 const updateTag = async (formEl: FormInstance | undefined): Promise<void> => {
-	formEl?.validate(async valid => {
+	formEl?.validate(async (valid) => {
 		if (valid) {
-			const { data, msg } = await updateTags(formData)
+			const { data, msg } = await updateTags(formData);
 
 			ElMessage({
 				type: data ? 'success' : 'error',
-				message: msg
-			})
+				message: msg,
+			});
 
 			for (const item of tableData.value) {
 				if (item.id === formData.id) {
-					Object.assign(item, formData)
-					break
+					Object.assign(item, formData);
+					break;
 				}
 			}
 		}
-	})
-}
+	});
+};
 
 // 编辑
 const editClick = (value: CreateTagDto): void => {
-	Object.assign(formData, value)
-	isEdit.value = true
-}
+	Object.assign(formData, value);
+	isEdit.value = true;
+};
 
 // 删除标签
 const deleteClick = (value: CreateTagDto): void => {
 	useConfirm('删除标签', 'warning', async () => {
-		const { data, msg } = await deleteTags(value.id!)
+		const { data, msg } = await deleteTags(value.id!);
 
 		ElMessage({
 			type: data ? 'success' : 'error',
-			message: msg
-		})
+			message: msg,
+		});
 
-		queryTag('', pageNumber.value)
-	})
-}
+		queryTag('', pageNumber.value);
+	});
+};
 
 // 分页查询
 const handleCurrentChange = (val: number): void => {
-	pageNumber.value = val
-	queryTag(searchTag.value, val)
-}
+	pageNumber.value = val;
+	queryTag(searchTag.value, val);
+};
 
 watch(searchTag, async () => {
-	queryTag(searchTag.value, 1)
-})
+	queryTag(searchTag.value, 1);
+});
 
-onMounted(async () => {
-	await queryTag(searchTag.value, 1)
-
+onMounted(() => {
 	nextTick(() => {
-		tableHeight.value = formRef.value?.offsetHeight || 0
-	})
-})
+		tableHeight.value = formRef.value?.offsetHeight || 0;
+
+		// 监听表格滚动
+		if (tableRef.value && tableRef.value.$el) {
+			const el = tableRef.value?.$el.querySelector('.el-scrollbar__wrap');
+			const { x, y } = useScroll(el);
+
+			watch([x, y], () => {
+				console.log('滚动了');
+			});
+		}
+	});
+});
 </script>
 
 <template>
-	<div class="w-full h-full">
+	<div class="h-full w-full">
 		<div ref="formRef" class="w-full">
-			<h3 class="font-bold mb-2">新增标签</h3>
+			<h3 class="mb-2 font-bold">新增标签</h3>
 			<ElForm ref="ruleFormRef" :inline="true" :model="formData" :rules="createTagRules" class="!w-full">
-				<ElFormItem class="!w-full sm:!w-[50%] !mx-0 sm:odd:pr-4" prop="name" label="名称：">
+				<ElFormItem class="!mx-0 !w-full sm:!w-[50%] sm:odd:pr-4" prop="name" label="名称：">
 					<ElInput v-model="formData.name" placeholder="请输入标签" clearable />
 				</ElFormItem>
-				<ElFormItem class="!w-full sm:!w-[50%] !mx-0 sm:odd:pr-4" prop="icon" label="图标：">
+				<ElFormItem class="!mx-0 !w-full sm:!w-[50%] sm:odd:pr-4" prop="icon" label="图标：">
 					<ElInput v-model="formData.icon" placeholder="请输入图标" clearable>
 						<template #prefix>
 							<Icon class="cursor-pointer text-[20px]"
@@ -160,30 +171,32 @@ onMounted(async () => {
 						</template>
 					</ElInput>
 				</ElFormItem>
-				<ElFormItem class="!w-full sm:!w-[50%] !mx-0 sm:odd:pr-4" prop="url" label="链接：">
+				<ElFormItem class="!mx-0 !w-full sm:!w-[50%] sm:odd:pr-4" prop="url" label="链接：">
 					<ElInput v-model="formData.url" placeholder="请输入链接" clearable />
 				</ElFormItem>
-				<ElFormItem class="!w-full sm:!w-[50%] !mx-0 sm:odd:pr-4" prop="type" label="类型：">
+				<ElFormItem class="!mx-0 !w-full sm:!w-[50%] sm:odd:pr-4" prop="type" label="类型：">
 					<ElSelect v-model="formData.type" placeholder="请选择" class="!w-full" clearable>
 						<ElOption label="博客" value="BLOG" />
 						<ElOption label="个人" value="PERSON" />
 					</ElSelect>
 				</ElFormItem>
-				<ElFormItem class="!w-full !mx-0 sm:pr-4">
-					<ElButton v-if="!isEdit" :disabled="isIDisabled" @click="createTag(ruleFormRef!)" type="primary">新增
+				<ElFormItem class="!mx-0 !w-full sm:pr-4">
+					<ElButton v-if="!isEdit" :disabled="isIDisabled" type="primary" @click="createTag(ruleFormRef!)">
+						新增
 					</ElButton>
-					<ElButton v-if="isEdit" :disabled="isIDisabled" type="primary" @click="updateTag(ruleFormRef!)">修改
+					<ElButton v-if="isEdit" :disabled="isIDisabled" type="primary" @click="updateTag(ruleFormRef!)">
+						修改
 					</ElButton>
 					<ElButton v-if="isEdit" type="" plain @click="isEdit = false">取消</ElButton>
 				</ElFormItem>
 			</ElForm>
 		</div>
 		<div class="w-full" :style="{ height: `calc(100% - ${tableHeight}px)` }">
-			<div class="font-bold mb-2 w-full flex justify-between items-center">
+			<div class="mb-2 flex w-full items-center justify-between font-bold">
 				<h3>标签列表</h3>
 				<ElInput v-model="searchTag" placeholder="请输入名称" clearable class="!w-[200px]" />
 			</div>
-			<ElTable :data="tableData" show-overflow-tooltip class="w-full !text-[16px] !border-none"
+			<ElTable ref="tableRef" :data="tableData" show-overflow-tooltip class="w-full !border-none !text-[16px]"
 				:class="['!h-[calc(100%-38px)] sm:!h-[calc(100%-70px)]']">
 				<ElTableColumn prop="name" label="名称" width="200" />
 				<ElTableColumn prop="icon" label="Icon" width="200">
@@ -207,11 +220,12 @@ onMounted(async () => {
 							编辑
 						</ElButton>
 						<ElButton link type="danger" class="!text-[16px]" size="small" @click="deleteClick(scope.row)">
-							删除</ElButton>
+							删除
+						</ElButton>
 					</template>
 				</ElTableColumn>
 			</ElTable>
-			<div ref="footerRef" class="hidden sm:flex h-[32px] w-full justify-end">
+			<div ref="footerRef" class="hidden h-[32px] w-full justify-end sm:flex">
 				<ElPagination background layout="prev, pager, next" :total="total" :page-size="10"
 					@current-change="handleCurrentChange" />
 			</div>
@@ -221,22 +235,22 @@ onMounted(async () => {
 
 <style lang="postcss" scoped>
 :deep(.el-form-item__content) {
-	@apply w-full flex justify-end
+	@apply flex w-full justify-end;
 }
 
 :deep(.el-form-item__label) {
-	@apply text-[16px] pr-0
+	@apply pr-0 text-[16px];
 }
 
 :deep(.el-input__wrapper) {
-	@apply text-[14px] h-[34px]
+	@apply h-[34px] text-[14px];
 }
 
 :deep(.el-select__wrapper) {
-	@apply text-[14px] h-[34px]
+	@apply h-[34px] text-[14px];
 }
 
 :deep(.el-table__inner-wrapper::before) {
-	@apply hidden
+	@apply hidden;
 }
 </style>
