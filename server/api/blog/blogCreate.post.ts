7e4e1adeb5ea0f68aa0cefc90ba8ed9ad3c603fc
huhai <2075313210@qq.com>
2025-10-13 21:43:17 +0800

    创建新增博客接口

---
+++ server/api/blog/blogCreate.post.ts
@@ -0,0 +1,21 @@
+import { container } from '~/server/core/container';
+import type { CreateBlogDto } from '~/server/dto/CreateBlogDto';
+import { CreateBlogSchema } from '~/server/dto/CreateBlogDto';
+import { BlogService } from '~/server/services/BlogService';
+
+export default defineEventHandler(async (event) => {
+	const blogService = container.get(BlogService);
+	const body = await readBody<CreateBlogDto>(event);
+
+	const result = validateData(CreateBlogSchema, body, (value: string) => {
+		sendErrorWithMessage(event, 400, value);
+		return null;
+	});
+
+	try {
+		return await blogService.createBlog(result);
+	} catch {
+		sendErrorWithMessage(event, 500, '创建失败！');
+		return null;
+	}
+});
