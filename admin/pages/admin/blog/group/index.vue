2faa397a55ad2c0ac41cf95eb38a514d2a015316
huhai <2075313210@qq.com>
2025-10-16 21:49:09 +0800

    创建标题

--- admin/pages/admin/blog/group/index.vue
+++ admin/pages/admin/blog/group/index.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup></script>
 
 <template>
-	<div>管理博客</div>
+	<div class="flex h-full w-full flex-col">
+		<h3 class="mb-2 font-bold">博客管理</h3>
+	</div>
 </template>
 
 <style lang="scss" scoped></style>
