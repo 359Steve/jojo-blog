6e3643ae61d0b98a9ea7664dd0b94ae707dcf7af
huhai <2075313210@qq.com>
2025-10-18 19:43:30 +0800

    自定义博客查询方法

--- admin/composables/api/blog/index.ts
+++ admin/composables/api/blog/index.ts
@@ -10,3 +10,14 @@ export const createBlog = async (data: CreateBlogDto) => {
 
 	return handleApiResponse(res);
 };
+
+// 查询博客列表
+export const getBlogList = async (data: FindBlogParams) => {
+	const res = await jojoLoadingIndicator(() =>
+		fetchUseGet<FindBlogParams, { records: CreateBlogDto[]; total: number }>('/blog/blogList', {
+			query: data,
+		}),
+	);
+
+	return handleApiResponse(res);
+};
