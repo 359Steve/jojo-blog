96a72bb1cb3cf67d120dbb647c42b2a781f73d6a
huhai <2075313210@qq.com>
2025-10-18 19:46:38 +0800

    创建查询博客接口

--- server/api/blog/blogList.get.ts
+++ server/api/blog/blogList.get.ts
@@ -3,16 +3,11 @@ import { BlogService } from '~/server/services/BlogService';
 
 export default defineEventHandler(async (event) => {
 	const blogService = container.get(BlogService);
-	const query = getQuery(event);
-
-	const page = Number(query.page) || 1;
-	const limit = Number(query.limit) || 10;
-	const keyword = query.keyword as string;
+	const query = getQuery<FindBlogParams>(event);
 
 	try {
-		return await blogService.getBlogList(page, limit, keyword);
+		return await blogService.getBlogList(query);
 	} catch (error) {
-		console.error('获取博客列表失败:', error);
 		sendErrorWithMessage(event, 500, '获取失败！');
 		return null;
 	}
