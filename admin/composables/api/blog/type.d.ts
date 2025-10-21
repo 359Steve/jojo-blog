ef1f355aacabbb4f5641f88423c605e61661d7b0
huhai <2075313210@qq.com>
2025-10-21 22:35:39 +0800

    创建博客与标签复合返回值类型

--- admin/composables/api/blog/type.d.ts
+++ admin/composables/api/blog/type.d.ts
@@ -3,3 +3,14 @@ interface FindBlogParams {
 	pageSize: number;
 	keyword?: string;
 }
+
+// 博客标签复合返回值
+interface BlogWithTagsDto<T> {
+	blog_id: number;
+	tag: T[];
+	tag_id: number;
+}
+
+type BlogWithTagsRep<T, P, K> = Omit<T, K> & {
+	tags: BlogWithTagsDto<P>[];
+};
