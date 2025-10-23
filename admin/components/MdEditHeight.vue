0b0416a9fbc2ab86f3989de2972c61a15a9a8255
huhai <2075313210@qq.com>
2025-10-23 15:59:40 +0800

    创建动态获取md高度组件

---
+++ admin/components/MdEditHeight.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const mdItem = useTemplateRef<HTMLDivElement>('MdItem');
+const mdHeight = computed(() => {
+	return mdItem.value ? mdItem.value.offsetHeight : 0;
+});
+</script>
+
+<template>
+	<div ref="MdItem" class="w-full flex-1">
+		<slot :height="mdHeight" />
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
