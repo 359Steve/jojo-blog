e4b64a09993611d372adcdba476882d4cd4b3b8c
huhai <2075313210@qq.com>
2025-10-23 23:10:29 +0800

    修改用户数据与标签联合类型

--- composables/api/user/index.ts
+++ composables/api/user/index.ts
@@ -3,7 +3,7 @@ import type { CreateUserDto } from '~/server/dto/CreateUserDto';
 
 // 查询用户公共信息
 export const findPublicUser = async () => {
-	const res = await useGet<any, Omit<UserInfoDetail<CreateUserDto, UserTags<CreateTagDto>[]>, 'password'>>(
+	const res = await useGet<any, UserWithTagsRep<Omit<CreateUserDto, 'password'>, CreateTagDto, 'tags'>>(
 		'/user/userPublicQuery',
 	);
 
