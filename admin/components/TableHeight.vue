a0fab191a129b901d6ebe66d1599af152e056f55
huhai <2075313210@qq.com>
2025-10-23 10:16:21 +0800

    创建表格父元素模版

---
+++ admin/components/TableHeight.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const tableRefs = useTemplateRef<HTMLDivElement>('tableRefs');
+const tableRefsHeight = ref<number>(0);
+
+onMounted(() => {
+	nextTick(() => {
+		if (tableRefs.value) {
+			tableRefsHeight.value = tableRefs.value?.offsetHeight || 0;
+		}
+	});
+});
+</script>
+
+<template>
+	<div ref="tableRefs" class="w-full flex-1">
+		<slot :height="tableRefsHeight" />
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
