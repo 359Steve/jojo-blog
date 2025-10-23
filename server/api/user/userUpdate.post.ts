53574a104f8bfc3236b45d6bf400a76688cf0fb2
huhai <2075313210@qq.com>
2025-10-23 23:12:21 +0800

    修改用户数据与标签联合类型

--- server/api/user/userUpdate.post.ts
+++ server/api/user/userUpdate.post.ts
@@ -1,10 +1,11 @@
 import { container } from '~/server/core/container';
+import type { CreateTagDto } from '~/server/dto/CreateTagDto';
 import { type CreateUserDto, CreateUserSchema } from '~/server/dto/CreateUserDto';
 import { UserService } from '~/server/services/UserService';
 
 export default defineEventHandler(async (event) => {
 	const userService = container.get(UserService);
-	const body = await readBody<UserInfoDetail<CreateUserDto, number[]>>(event);
+	const body = await readBody<CreateUserDto & { tags: number[] }>(event);
 
 	const result = validateData(CreateUserSchema, body, (value: string) => {
 		sendErrorWithMessage(event, 400, value);
