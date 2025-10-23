39334c2c29450020eb0595cdbde02b6fb50d1150
huhai <2075313210@qq.com>
2025-10-23 14:54:04 +0800

    注入md

--- server/core/container.ts
+++ server/core/container.ts
@@ -5,6 +5,8 @@ import { TagRepository } from '../repositories/TagRepository';
 import { TagService } from '../services/TagService';
 import { BlogService } from '../services/BlogService';
 import { BlogRepository } from '../repositories/BlogRepository';
+import { MdService } from '../services/MdService';
+import { MdRepository } from '../repositories/MdRepository';
 
 const container = new Container();
 container.bind(UserRepository).toSelf();
@@ -13,5 +15,7 @@ container.bind(TagRepository).toSelf();
 container.bind(TagService).toSelf();
 container.bind(BlogRepository).toSelf();
 container.bind(BlogService).toSelf();
+container.bind(MdService).toSelf();
+container.bind(MdRepository).toSelf();
 
 export { container };
