1015b19808799387f567ba0ef0c7e8f6ee631304
huhai <2075313210@qq.com>
2025-10-28 17:25:37 +0800

    完善布局

--- components/index/BasicTime.vue
+++ components/index/BasicTime.vue
@@ -14,9 +14,9 @@ defineEmits<{
 			<div class="relative h-full w-full"
 				:class="[(index + 1) % 2 === 0 ? 'col-start-2 pl-4' : 'col-start-1 pr-4']">
 				<div class="absolute size-3 rounded-[50%] bg-[#E4E7EC]"
-					:class="[(index + 1) % 2 === 0 ? 'left-[calc(-0.75rem/2)]' : 'right-[calc(-0.75rem/2)]']"></div>
+					:class="[(index + 1) % 2 === 0 ? 'left-[calc(-0.75rem/2)]' : 'right-[calc(-0.75rem/2)]']" />
 				<div class="absolute h-full w-[4px] bg-[#E4E7EC]"
-					:class="[(index + 1) % 2 === 0 ? 'left-[-2px]' : 'right-[-2px]']"></div>
+					:class="[(index + 1) % 2 === 0 ? 'left-[-2px]' : 'right-[-2px]']" />
 				<div class="h-full w-full">
 					<div class="flex w-full items-center text-xs text-gray-400"
 						:class="[(index + 1) % 2 === 0 ? 'justify-start' : 'justify-end']">
@@ -27,8 +27,8 @@ defineEmits<{
 						<div class="mt-2 flex cursor-pointer items-center rounded-base p-2 shadow-lg backdrop-blur transition-all hover:bg-gray-50 dark:shadow-[0_4px_20px_rgba(255,255,255,0.05)] dark:hover:bg-gray-100/10"
 							:class="[(index + 1) % 2 === 0 ? 'text-left' : 'text-right']"
 							@click="$emit('detailRecord', item)">
-							<img :src="item.url" alt="timestamp" class="mr-2 size-8" />
-							<div class="h-full w-[calc(100%-2rem)] break-normal text-center">
+							<img :src="item.url" alt="timestamp" class="mr-2 size-8">
+							<div class="line-clamp-2 h-full w-[calc(100%-2rem)] overflow-hidden text-center">
 								<span>{{ item.description }}</span>
 							</div>
 						</div>
