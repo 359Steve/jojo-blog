553979020e0798c9bb01e3d73f5d5682ab7a4d01
huhai <2075313210@qq.com>
2025-10-29 15:55:40 +0800

    创建用户信息状态管理

---
+++ composables/blogUserInfo.ts
@@ -0,0 +1,20 @@
+import type { CreateTagDto } from '~/server/dto/CreateTagDto';
+import type { CreateUserDto } from '~/server/dto/CreateUserDto';
+
+export const useBlogUserInfo = defineStore('blogUserInfo', () => {
+	const userInfo = ref<UserWithTagsRep<Omit<CreateUserDto, 'password'>, CreateTagDto, 'tags'> | null>(null);
+
+	const getBlogUserInfo = () => {
+		return userInfo.value;
+	};
+
+	const setBlogUserInfo = (info: UserWithTagsRep<Omit<CreateUserDto, 'password'>, CreateTagDto, 'tags'>) => {
+		userInfo.value = info;
+	};
+
+	return {
+		userInfo,
+		getBlogUserInfo,
+		setBlogUserInfo,
+	};
+});
