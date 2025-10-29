266b1f4398f1e0504ff6b35b517f289539bb7cdd
huhai <2075313210@qq.com>
2025-10-29 21:20:08 +0800

    修改输入框样式

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -13,53 +13,17 @@ const { data } = await useAsyncData('publicQueryBlogList', () =>
 );
 const blogList = ref<BlogWithTagsRep<CreateBlogDto, CreateTagDto, 'tags'>[]>(data.value?.data?.records || []);
 const search = ref<string>('');
-const inputEl = ref<any>(null);
-const animationEl = ref<any>(null);
-const coincidence = ref<boolean>(true);
-
-let parentBounds: ReturnType<typeof useElementBounding>;
-let childBounds: ReturnType<typeof useElementBounding>;
 
 const toDetail = (blog: BlogWithTagsRep<CreateBlogDto, CreateTagDto, 'tags'>): void => {
 	useBlog().setCurrentBlog(blog);
 	navigateTo({ path: '/blog/detail', query: { id: blog.id } });
 };
-
-const isLeftAligned = computed(() => {
-	if (!parentBounds || !childBounds) return false;
-	return Math.abs(childBounds.left.value - parentBounds.left.value) < 1;
-});
-
-const onTransitionEnd = (event: TransitionEvent) => {
-	if (event.propertyName !== 'transform') return;
-	coincidence.value = isLeftAligned.value;
-};
-
-onMounted(() => {
-	nextTick(() => {
-		const input_el: HTMLElement = inputEl.value!.$el;
-		parentBounds = useElementBounding(animationEl.value!.el);
-		childBounds = useElementBounding(inputEl.value!.$el);
-		input_el.addEventListener('transitionend', onTransitionEnd);
-	});
-});
-
-onBeforeUnmount(() => {
-	const input_el: HTMLElement = inputEl.value!.$el;
-	input_el.removeEventListener('transitionend', onTransitionEnd);
-});
-
-watch(coincidence, () => {
-	if (!coincidence.value) {
-		inputEl.value?.focus();
-	}
-});
 </script>
 
 <template>
-	<AnimationRevealOnScroll ref="animationEl" animation-class="animate__fadeInDown"
-		base-class="reactive mb-4 flex items-center justify-between overflow-hidden rounded-base">
-		<ElInput id="elinput" ref="inputEl" v-model="search" placeholder="请输入搜索关键字" size="large" />
+	<AnimationRevealOnScroll animation-class="animate__fadeInDown"
+		base-class="mb-4 sm:mb-8 overflow-hidden rounded-base">
+		<ElInput v-model="search" placeholder="请输入搜索关键字" size="large" />
 	</AnimationRevealOnScroll>
 	<div class="w-full">
 		<div class="grid grid-cols-1 gap-10 lg:grid-cols-2">
@@ -115,22 +79,11 @@ watch(coincidence, () => {
 	@apply h-full w-full;
 }
 
-:deep(.el-input-group__append) {
-	@apply aspect-square h-full cursor-pointer rounded-bl-none rounded-br-base rounded-tl-none rounded-tr-base border-none bg-neutral-100 p-0 shadow-none;
-}
-
 :deep(.el-input__wrapper) {
-	@apply rounded-bl-base rounded-br-none rounded-tl-base rounded-tr-none border-none bg-neutral-100 px-2 py-2 shadow-none ring-0 focus:outline-none focus:ring-0;
-}
-
-:deep(.el-input__wrapper:hover),
-:deep(.el-input__wrapper.is-focus),
-:deep(.el-input__wrapper:focus),
-:deep(.el-input__wrapper.is-hover) {
-	@apply border-none shadow-none outline-none ring-0;
+	@apply rounded-base bg-white px-3 py-2 shadow-lg dark:bg-gray-100/10 sm:shadow-none;
 }
 
 :deep(.el-input__inner) {
-	@apply border-none bg-transparent text-sm text-neutral-700 shadow-none ring-0 focus:outline-none focus:ring-0;
+	@apply text-[16px];
 }
 </style>
