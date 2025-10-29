2830e7e7fd22d86f4b662f1587926247715f045d
huhai <2075313210@qq.com>
2025-10-29 15:55:49 +0800

    删除原来的用户信息状态管理

--- composables/vueStarport.ts
+++ composables/vueStarport.ts
@@ -1,18 +1,6 @@
-import { defineStore } from 'pinia';
-import type { CreateTagDto } from '~/server/dto/CreateTagDto';
-import type { CreateUserDto } from '~/server/dto/CreateUserDto';
-
 export const useVueStarport = defineStore(
 	'vueStarport',
 	() => {
-		const userInfo = reactive<UserWithTagsRep<Omit<CreateUserDto, 'password'>, CreateTagDto, 'tags'>>({
-			user_name: '',
-			pet_name: '',
-			sign: '',
-			describe: '',
-			avatar_url: '',
-			tags: [],
-		});
 		const isRound = ref<boolean>(false);
 
 		const getIsRound = (): boolean => {
@@ -23,19 +11,8 @@ export const useVueStarport = defineStore(
 			isRound.value = value;
 		};
 
-		const getUserInfo = (): UserWithTagsRep<Omit<CreateUserDto, 'password'>, CreateTagDto, 'tags'> => {
-			return userInfo;
-		};
-
-		const setUserInfo = (value: UserWithTagsRep<Omit<CreateUserDto, 'password'>, CreateTagDto, 'tags'>): void => {
-			Object.assign(userInfo, value);
-		};
-
 		return {
 			isRound,
-			userInfo,
-			getUserInfo,
-			setUserInfo,
 			setIsRound,
 			getIsRound,
 		};
