9f12eb292e3da76359b11c9fd5bfd04300f49326
huhai <2075313210@qq.com>
2025-10-30 20:48:37 +0800

    注入履历分组和履历详情

--- server/core/container.ts
+++ server/core/container.ts
@@ -7,6 +7,10 @@ import { BlogService } from '../services/BlogService';
 import { BlogRepository } from '../repositories/BlogRepository';
 import { MdService } from '../services/MdService';
 import { MdRepository } from '../repositories/MdRepository';
+import { ArticleService } from '../services/ArticleService';
+import { ArticleRepository } from '../repositories/ArticleRepository';
+import { GroupService } from '../services/GroupService';
+import { GroupRepository } from '../repositories/GroupRepository';
 
 const container = new Container();
 container.bind(UserRepository).toSelf();
@@ -17,5 +21,9 @@ container.bind(BlogRepository).toSelf();
 container.bind(BlogService).toSelf();
 container.bind(MdService).toSelf();
 container.bind(MdRepository).toSelf();
+container.bind(ArticleService).toSelf();
+container.bind(ArticleRepository).toSelf();
+container.bind(GroupService).toSelf();
+container.bind(GroupRepository).toSelf();
 
 export { container };
