0cef8bcbcba605fbd3e4013a0ed9972a1d923e4f
huhai <2075313210@qq.com>
2025-10-31 10:14:40 +0800

    完善首页菜单id

--- middleware/index.global.ts
+++ middleware/index.global.ts
@@ -1,15 +1,20 @@
 export default defineNuxtRouteMiddleware((to, _from) => {
-	to.path = normalizePath(to.path);
-	const path = to.path;
-
 	const { menuList, menuId } = storeToRefs(useJojoHeader());
 
-	// 优先匹配最长的路径（避免 / 匹配到所有路径）
-	const matched = menuList.value
-		.filter((item) => path === item.path || path.startsWith(`${item.path}/`))
-		.sort((a, b) => b.path.length - a.path.length)[0];
+	if (to.path === '/') {
+		menuId.value = 1;
+		return true;
+	} else {
+		to.path = normalizePath(to.path);
+		const path = to.path;
 
-	menuId.value = matched ? matched.id : 0;
+		// 优先匹配最长的路径（避免 / 匹配到所有路径）
+		const matched = menuList.value
+			.filter((item) => path === item.path || path.startsWith(`${item.path}/`))
+			.sort((a, b) => b.path.length - a.path.length)[0];
+
+		menuId.value = matched ? matched.id : 0;
+	}
 
 	return true;
 });
