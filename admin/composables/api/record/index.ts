eacc5c28860bae5ac7dbf6ca0ad18e0b40dc8865
huhai <2075313210@qq.com>
2025-11-03 18:25:10 +0800

    封装查询分组方法

--- admin/composables/api/record/index.ts
+++ admin/composables/api/record/index.ts
@@ -1,5 +1,19 @@
 import type { CreateGroupDto } from '~/server/dto/CreateGroupDto';
 
+// 查询全部分组
+export const queryGroupAll = async (page: number = 1, size: number = 10) => {
+	const res = await jojoLoadingIndicator(() =>
+		fetchUseGet<{ pageNumber: number; pageSize: number }, { records: CreateGroupDto[]; total: number }>(
+			'/record/group/groupQueryAll',
+			{
+				query: { pageNumber: page, pageSize: size },
+			},
+		),
+	);
+
+	return handleApiResponse(res);
+};
+
 // 新增分组
 export const createGroup = async (data: CreateGroupDto) => {
 	const res = await jojoLoadingIndicator(() =>
