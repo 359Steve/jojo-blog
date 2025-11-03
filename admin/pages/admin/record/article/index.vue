daae2605275ae9bd79343877c722169677e3f981
huhai <2075313210@qq.com>
2025-11-03 17:21:53 +0800

    搭建文章管理页面

--- admin/pages/admin/record/article/index.vue
+++ admin/pages/admin/record/article/index.vue
@@ -1,7 +1,12 @@
 <script lang="ts" setup></script>
 
 <template>
-	<div>文章管理</div>
+	<div class="flex h-full w-full flex-col">
+		<div class="w-full">
+			<h3 class="mb-2 font-bold">文章管理</h3>
+			<!-- 这里是内容区 -->
+		</div>
+	</div>
 </template>
 
 <style lang="scss" scoped></style>
