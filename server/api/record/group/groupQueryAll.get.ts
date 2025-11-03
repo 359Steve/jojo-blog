0ceef23cdcdc5f5afb0838275bf200ce3a83c754
huhai <2075313210@qq.com>
2025-11-03 18:23:47 +0800

    添加查询分组接口

---
+++ server/api/record/group/groupQueryAll.get.ts
@@ -0,0 +1,17 @@
+import { container } from '~/server/core/container';
+import { GroupService } from '~/server/services/GroupService';
+
+export default defineEventHandler(async (event) => {
+	const query = getQuery<{ pageNumber: string; pageSize: string }>(event);
+
+	try {
+		const groupService = container.get(GroupService);
+		return await groupService.getAllGroups({
+			pageNumber: Number(query.pageNumber) || 1,
+			pageSize: Number(query.pageSize) || 10,
+		});
+	} catch {
+		sendErrorWithMessage(event, 500, '查询失败！');
+		return null;
+	}
+});
