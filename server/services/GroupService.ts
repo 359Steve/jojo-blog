54b6bcdb59c6dc6cb22d8f98729fea896d393d50
huhai <2075313210@qq.com>
2025-11-03 17:23:49 +0800

    创建分组service层

--- server/services/GroupService.ts
+++ server/services/GroupService.ts
@@ -1,1 +1,14 @@
-export class GroupService { }
+import type { CreateGroupDto } from '../dto/CreateGroupDto';
+import { GroupRepository } from '../repositories/GroupRepository';
+
+export class GroupService {
+	private groupRepo: GroupRepository;
+	constructor() {
+		this.groupRepo = new GroupRepository();
+	}
+
+	// 新增分组
+	async createGroup(data: CreateGroupDto) {
+		return await this.groupRepo.createGroup(data);
+	}
+}
