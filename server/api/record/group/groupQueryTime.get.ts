c207c7f9fdbb432e19f07d83f3712e4fc1a14571
huhai <2075313210@qq.com>
2025-11-05 18:19:20 +0800

    创建查询分组日期列表接口

---
+++ server/api/record/group/groupQueryTime.get.ts
@@ -0,0 +1,12 @@
+import { container } from '~/server/core/container';
+import { GroupService } from '~/server/services/GroupService';
+
+export default defineEventHandler(async (event) => {
+	try {
+		const groupService = container.get(GroupService);
+		return await groupService.getGroupTimeRanges();
+	} catch {
+		sendErrorWithMessage(event, 500, '查询失败！');
+		return null;
+	}
+});
