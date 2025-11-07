33644967ae653556a6b8744a7e6c3de5f13bf2e0
huhai <2075313210@qq.com>
2025-11-07 17:56:34 +0800

    使用动态数据

--- components/index/BasicTime.vue
+++ components/index/BasicTime.vue
@@ -1,16 +1,19 @@
 <script lang="ts" setup>
+import type { CreateRecordDetailDto } from '~/server/dto/CreateArticleDto';
+
 const { timelineData = [] } = defineProps<{
-	timelineData: Timeline[];
+	timelineData: CreateRecordDetailDto[];
 }>();
 
-defineEmits<{
-	(e: 'detailRecord', value: Timeline): void;
-}>();
+const toDetail = (groupId: number, recordId: number) => {
+	navigateTo({ path: `/record/detail/${groupId}/${recordId}` });
+};
 </script>
 
 <template>
 	<div class="h-full w-full">
-		<div v-for="(item, index) in timelineData" :key="item.id" class="grid grid-cols-2">
+		<div v-for="(item, index) in timelineData" :key="item.id" class="grid grid-cols-2"
+			@click="toDetail(item.group_id, item.id!)">
 			<div class="relative h-full w-full"
 				:class="[(index + 1) % 2 === 0 ? 'col-start-2 pl-4' : 'col-start-1 pr-4']">
 				<div class="absolute size-3 rounded-[50%] bg-[#E4E7EC]"
@@ -20,16 +23,16 @@ defineEmits<{
 				<div class="h-full w-full">
 					<div class="flex w-full items-center text-xs text-gray-400"
 						:class="[(index + 1) % 2 === 0 ? 'justify-start' : 'justify-end']">
-						<span>{{ item.timestamp }}</span>
+						<span>{{ item.time_range }}</span>
 					</div>
 					<AnimationRevealOnScroll
 						:animation-class="(index + 1) % 2 === 0 ? 'animate__fadeInRight' : 'animate__fadeInLeft'">
 						<div class="mt-2 flex cursor-pointer items-center rounded-base p-2 shadow-lg backdrop-blur transition-all hover:bg-gray-50 dark:shadow-[0_4px_20px_rgba(255,255,255,0.05)] dark:hover:bg-gray-100/10"
 							:class="[(index + 1) % 2 === 0 ? 'text-left' : 'text-right']"
 							@click="$emit('detailRecord', item)">
-							<img :src="item.url" alt="timestamp" class="mr-2 size-8">
+							<img :src="item.image_url" :alt="item.image_alt" class="mr-2 size-8">
 							<div class="line-clamp-2 h-full w-[calc(100%-2rem)] overflow-hidden text-center">
-								<span>{{ item.description }}</span>
+								<span>{{ item.title }}</span>
 							</div>
 						</div>
 					</AnimationRevealOnScroll>
--- components/index/MdToLgTime.vue
+++ components/index/MdToLgTime.vue
@@ -1,28 +1,31 @@
 <script lang="ts" setup>
+import type { CreateRecordDetailDto } from '~/server/dto/CreateArticleDto';
+
 const { timelineData = [] } = defineProps<{
-	timelineData: Timeline[];
+	timelineData: CreateRecordDetailDto[];
 }>();
 
-defineEmits<{
-	(e: 'detailRecord', value: Timeline): void;
-}>();
+const toDetail = (groupId: number, recordId: number) => {
+	navigateTo({ path: `/record/detail/${groupId}/${recordId}` });
+};
 </script>
 
 <template>
 	<div class="h-full w-full">
-		<div v-for="item in timelineData" :key="item.id" class="grid grid-cols-2 md:grid-cols-1 lg:grid-cols-2">
+		<div v-for="item in timelineData" :key="item.id" class="grid grid-cols-2 md:grid-cols-1 lg:grid-cols-2"
+			@click="toDetail(item.group_id, item.id!)">
 			<div class="relative h-full w-full">
-				<div class="absolute left-[calc(-0.75rem/2)] size-3 rounded-[50%] bg-[#E4E7EC]"></div>
-				<div class="absolute left-[-2px] h-full w-[4px] bg-[#E4E7EC]"></div>
+				<div class="absolute left-[calc(-0.75rem/2)] size-3 rounded-[50%] bg-[#E4E7EC]" />
+				<div class="absolute left-[-2px] h-full w-[4px] bg-[#E4E7EC]" />
 				<div class="ml-2 h-full w-full">
 					<div class="flex w-full items-center text-xs text-gray-400">
-						<span>{{ item.timestamp }}</span>
+						<span>{{ item.time_range }}</span>
 					</div>
 					<AnimationRevealOnScroll animation-class="'animate__fadeInRight'">
 						<div class="mt-2 flex cursor-pointer items-center rounded-base p-2 shadow-lg transition-all hover:bg-gray-50 dark:shadow-[0_4px_20px_rgba(255,255,255,0.05)] dark:hover:bg-gray-100/10"
 							@click="$emit('detailRecord', item)">
-							<img :src="item.url" alt="timestamp" class="mr-2 size-8" />
-							<span>{{ item.description }}</span>
+							<img :src="item.image_url" :alt="item.image_alt" class="mr-2 size-8">
+							<span>{{ item.title }}</span>
 						</div>
 					</AnimationRevealOnScroll>
 				</div>
