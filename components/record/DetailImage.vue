f8a06f875cf64b9b6b2d0f8762f393705546e648
huhai <2075313210@qq.com>
2025-11-07 16:57:14 +0800

    添加动态数据

--- components/record/DetailImage.vue
+++ components/record/DetailImage.vue
@@ -1,7 +1,12 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const { img_url, img_alt } = defineProps<{
+	img_url: string;
+	img_alt?: string;
+}>();
+</script>
 
 <template>
-	<img class="h-full w-full rounded-base object-cover" src="~/assets/image/index_two.png" alt="" />
+	<img class="h-full w-full rounded-base object-cover" :src="img_url" :alt="img_alt" />
 </template>
 
 <style lang="scss" scoped></style>
