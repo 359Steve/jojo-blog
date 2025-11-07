62b6525a9ade9b5977975cde400d5b5b093547a2
huhai <2075313210@qq.com>
2025-11-07 17:56:51 +0800

    使用chooseCondition方法判断环境

--- composables/api/user/index.ts
+++ composables/api/user/index.ts
@@ -1,11 +1,12 @@
+import type { CreateRecordDetailDto } from '~/server/dto/CreateArticleDto';
 import type { CreateTagDto } from '~/server/dto/CreateTagDto';
 import type { CreateUserDto } from '~/server/dto/CreateUserDto';
 
 // 查询用户公共信息
-export const findPublicUser = async () => {
-	const res = await useGet<any, UserWithTagsRep<Omit<CreateUserDto, 'password'>, CreateTagDto, 'tags'>>(
-		'/user/userPublicQuery',
-	);
+export const findPublicUser = chooseCondition<
+	any,
+	UserWithTagsRep<Omit<CreateUserDto, 'password'>, CreateTagDto, 'tags'>
+>('/user/userPublicQuery');
 
-	return handleApiResponse(res);
-};
+// 查询首页推荐的记录详情
+export const queryRecordHomeLine = chooseCondition<any, CreateRecordDetailDto[]>('/record/recordHomeLine');
