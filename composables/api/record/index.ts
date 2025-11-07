13531ff87e8061608a820090e61465d58f9c9cc5
huhai <2075313210@qq.com>
2025-11-07 16:58:25 +0800

    封装查询记录方法

--- composables/api/record/index.ts
+++ composables/api/record/index.ts
@@ -2,8 +2,16 @@ import type { CreateRecordDetailDto } from '~/server/dto/CreateArticleDto';
 import type { CreateGroupDto } from '~/server/dto/CreateGroupDto';
 
 // 查询全部分组
-export async function recordPublicQuery() {
-	return await chooseCondition<{ keyword?: string }, RecordWithDetailsRep<CreateGroupDto, CreateRecordDetailDto>[]>(
-		'/record/recordPublicQuery',
-	)();
-}
+export const recordPublicQuery = chooseCondition<
+	{ keyword?: string },
+	RecordWithDetailsRep<CreateGroupDto, CreateRecordDetailDto>[]
+>('/record/recordPublicQuery');
+
+// 查询单个记录详情
+export const recordDetailQuery = (parentId: number, id: number) =>
+	chooseCondition<{ parentId: number; id: number }, RecordDetailRep<CreateRecordDetailDto>>(
+		'/record/recordPublicDetail',
+	)({
+		parentId,
+		id,
+	});
