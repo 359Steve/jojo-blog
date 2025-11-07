2841680b78bc47040e8b60796b5faf3f6523b04f
huhai <2075313210@qq.com>
2025-11-07 16:58:42 +0800

    添加查询记录接口返回值类型

--- composables/api/record/type.d.ts
+++ composables/api/record/type.d.ts
@@ -1,3 +1,9 @@
 type RecordWithDetailsRep<T, K> = T & {
-	details: K[];
+	details: Pick<K, 'id' | 'title' | 'image_url' | 'image_alt'>[];
+};
+
+type RecordDetailRep<T> = T & {
+	imageAll: Pick<T, 'id' | 'image_url' | 'image_alt'>[];
+	prev: { id: number } | null;
+	next: { id: number } | null;
 };
