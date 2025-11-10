a6fd41b27631db4380a9fa36f037208392c93294
huhai <2075313210@qq.com>
2025-11-10 15:15:26 +0800

    修改后台管理系统logo

--- admin/components/aside/LaySidebarLogo.vue
+++ admin/components/aside/LaySidebarLogo.vue
@@ -1,10 +1,12 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const { Avatar } = getIcons();
+</script>
 
 <template>
 	<div class="relative h-[48px] w-full overflow-hidden">
 		<Transition name="sidebarLogoFade">
 			<RouterLink key="expand" class="flex h-full flex-nowrap items-center pl-[10px]" to="/admin">
-				<img src="../../assets/svg/logo.svg" alt="logo" />
+				<Avatar />
 				<span
 					class="m-[2px_0_0_12px] inline-block h-[32px] overflow-hidden text-ellipsis whitespace-nowrap text-[18px] font-[600] leading-[32px] text-admin-sub-menu-active-text">
 					JoJoAdmin
