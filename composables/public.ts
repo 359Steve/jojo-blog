4a5b0635a7f4b8df98fcbf040b0f439974311e54
huhai <2075313210@qq.com>
2025-11-10 17:25:54 +0800

    修改密码正则

--- composables/public.ts
+++ composables/public.ts
@@ -65,8 +65,7 @@ export const useConfirm = (
 };
 
 /** 密码正则（密码格式应为8-18位数字、字母、符号的任意两种组合） */
-export const REGEXP_PWD =
-	/^(?![0-9]+$)(?![a-z]+$)(?![A-Z]+$)(?!([^(0-9a-zA-Z)]|[()])+$)(?!^.*[\u4E00-\u9FA5].*$)([^(0-9a-zA-Z)]|[()]|[a-z]|[A-Z]|[0-9]){8,18}$/;
+export const REGEXP_PWD = /^\S*(?=\S{6,})(?=\S*\d)(?=\S*[A-Z])(?=\S*[a-z])(?=\S*[!@#$%^&*? ])\S*$/;
 
 // 封装useLoadingIndicator
 export const jojoLoadingIndicator = async <T>(fn: () => Promise<T>): Promise<T> => {
