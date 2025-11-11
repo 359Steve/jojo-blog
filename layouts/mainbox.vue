d9bf44fda82e2fc4cd9093b7f832c55700c9e267
huhai <2075313210@qq.com>
2025-11-11 14:59:29 +0800

    格式化代码

--- layouts/mainbox.vue
+++ layouts/mainbox.vue
@@ -2,7 +2,7 @@
 
 <template>
 	<div class="m-auto box-border h-auto w-full p-4 mobile-pad:max-w-xl sm:max-w-2xl md:max-w-3xl lg:max-w-4xl">
-		<slot name="childPage"></slot>
+		<slot name="childPage" />
 	</div>
 </template>
 
