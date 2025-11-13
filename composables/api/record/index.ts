0672b5fc28dbec1f60082e44f0ede9d1d71ebdea
huhai <2075313210@qq.com>
2025-11-13 20:09:11 +0800

    查询分组修改成单个查询

--- composables/api/record/index.ts
+++ composables/api/record/index.ts
@@ -2,10 +2,13 @@ import type { CreateRecordDetailDto } from '~/server/dto/CreateArticleDto';
 import type { CreateGroupDto } from '~/server/dto/CreateGroupDto';
 
 // 查询全部分组
-export const recordPublicQuery = chooseCondition<
-	{ keyword?: string },
-	RecordWithDetailsRep<CreateGroupDto, CreateRecordDetailDto>[]
->('/record/recordPublicQuery');
+export const recordPublicQuery = (id?: number, keyword?: string) =>
+	chooseCondition<{ id?: number; keyword?: string }, RecordWithDetailsRep<CreateGroupDto, CreateRecordDetailDto>>(
+		'/record/recordPublicQuery',
+	)({
+		id,
+		keyword,
+	});
 
 // 查询单个记录详情
 export const recordDetailQuery = (parentId: number, id: number) =>
