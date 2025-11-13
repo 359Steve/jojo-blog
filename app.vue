f8ff443d4d16b704bded9b2cbaa3f9ae6c54a39c
huhai <2075313210@qq.com>
2025-11-13 20:03:37 +0800

    删除使用原生监听浏览器滚动

--- app.vue
+++ app.vue
@@ -1,18 +1,5 @@
 <script lang="ts" setup>
-const scrollY = ref<number>(0);
-
-const handleScroll = () => {
-	const scrollTop = window.scrollY || document.documentElement.scrollTop;
-	useJojoHeader().setScroll(scrollTop > scrollY.value && scrollTop > useJojoHeader().getHeaderHeight());
-	scrollY.value = scrollTop;
-};
-
-onMounted(() => {
-	window.addEventListener('scroll', handleScroll);
-});
-
 onBeforeUnmount(() => {
-	window.removeEventListener('scroll', handleScroll);
 	// 还原数据
 	useAdminMenu().reset();
 	useVueStarport().setIsRound(false);
