dcea84a49f99e7bd0699962ee7e2a575fa9e3329
huhai <2075313210@qq.com>
2025-11-13 20:09:27 +0800

    修改查询分组返回值类型

--- composables/api/record/type.d.ts
+++ composables/api/record/type.d.ts
@@ -1,5 +1,7 @@
 type RecordWithDetailsRep<T, K> = T & {
-	details: Pick<K, 'id' | 'title' | 'image_url' | 'image_alt'>[];
+	prev: { id: number } | null;
+	next: { id: number } | null;
+	details: Pick<K, 'id' | 'title' | 'image_url' | 'image_alt' | 'time_range'>[];
 };
 
 type RecordDetailRep<T> = T & {
