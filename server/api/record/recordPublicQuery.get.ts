08b2d8b6c233a94c8349a210c7e6fad023f91615
huhai <2075313210@qq.com>
2025-11-13 20:12:02 +0800

    修改参数

--- server/api/record/recordPublicQuery.get.ts
+++ server/api/record/recordPublicQuery.get.ts
@@ -2,11 +2,11 @@ import { container } from '~/server/core/container';
 import { GroupService } from '~/server/services/GroupService';
 
 export default defineEventHandler(async (event) => {
-	const query = getQuery<{ keyword?: string }>(event);
+	const query = getQuery<{ id?: number; keyword?: string }>(event);
 
 	try {
 		const groupService = container.get(GroupService);
-		return await groupService.getPublicGroups(query.keyword);
+		return await groupService.getPublicGroups(query);
 	} catch (error) {
 		sendErrorWithMessage(event, 500, '分组列表查询失败');
 		return null;
