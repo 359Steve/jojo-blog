df1855c43f64936cb9bdf7698fd4bfca2323960c
huhai <2075313210@qq.com>
2025-11-13 20:07:50 +0800

    修改样式

--- components/record/RecordHeader.vue
+++ components/record/RecordHeader.vue
@@ -1,11 +1,17 @@
 <script lang="ts" setup>
+const { space = true, sm = true } = defineProps<{
+	space?: boolean;
+	sm?: boolean;
+}>();
 const { userInfo } = storeToRefs(useBlogUserInfo());
 </script>
 
 <template>
-	<div class="mb-12 w-full sm:mb-20">
-		<div class="relative flex flex-col space-x-0 sm:flex-row sm:space-x-10">
-			<div class="mb-4 flex w-full items-center justify-center sm:mb-0 sm:w-40 sm:justify-normal">
+	<div class="mb-12 w-full">
+		<div class="relative flex flex-col space-x-0"
+			:class="[space ? 'sm:space-x-10' : 'lg:space-x-10', sm ? 'sm:flex-row' : 'lg:flex-row']">
+			<div class="mb-4 flex w-full items-center justify-center sm:w-auto"
+				:class="[sm ? 'sm:mb-0 sm:justify-normal' : 'lg:mb-0 lg:justify-normal']">
 				<Starport id="record-image-my-id" port="my-id"
 					class="aspect-square h-20 w-20 md:h-24 md:w-24 lg:h-28 lg:w-28 xl:h-32 xl:w-32">
 					<RecordImage class="rounded-[50%] transition-all duration-1000" />
@@ -14,7 +20,7 @@ const { userInfo } = storeToRefs(useBlogUserInfo());
 			<div class="grid">
 				<AnimationRevealOnScroll animation-class="'animate__fadeInDown'">
 					<h1
-						class="from-primary to-secondary w-full bg-gradient-to-r bg-clip-text text-base font-black sm:text-xl md:text-2xl lg:text-4xl">
+						class="from-primary to-secondary w-full bg-gradient-to-r bg-clip-text text-base font-black text-rose-500 sm:text-xl md:text-2xl lg:text-3xl">
 						{{ userInfo?.pet_name }}
 					</h1>
 				</AnimationRevealOnScroll>
@@ -26,7 +32,7 @@ const { userInfo } = storeToRefs(useBlogUserInfo());
 					<AnimationRevealOnScroll v-for="item in userInfo?.tags" :key="item.tag_id"
 						animation-class="'animate__fadeInDown'" base-class="'w-fit'">
 						<a :href="item.tag.url" target="_blank" rel="noopener noreferrer" class="button">
-							<ElButton size="default">
+							<ElButton size="default" class="!text-black dark:!text-white">
 								<template #icon>
 									<div class="text-[20px]">
 										<Icon :icon="item.tag.icon" />
