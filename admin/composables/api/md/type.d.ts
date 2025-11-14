312b890548104d27903698311b3b9925f7cafcd0
huhai <2075313210@qq.com>
2025-11-14 16:09:29 +0800

    添加类型

--- admin/composables/api/md/type.d.ts
+++ admin/composables/api/md/type.d.ts
@@ -9,4 +9,17 @@ interface MdUploadImageFile {
 interface MdUploadImageResponse {
 	urls: string[];
 	files: MdUploadImageFile[];
+	datePath: string;
+}
+
+// 删除指定目录下的图片返回值类型
+interface MdDeleteImageDirResponse {
+	deletedFiles: string[];
+	notFoundFiles: string[];
+}
+
+// 删除指定目录下的图片请求参数类型
+interface MdDeleteImageDirRequest {
+	datePath: string;
+	fileNames: string[];
 }
