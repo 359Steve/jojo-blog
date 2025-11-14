f9d2ddc2e1d9efdc11d95497f82204b1776fb502
huhai <2075313210@qq.com>
2025-11-14 16:11:24 +0800

    添加博客文章图标标识字段

--- server/dto/CreateBlogDto.ts
+++ server/dto/CreateBlogDto.ts
@@ -22,6 +22,11 @@ export const CreateBlogSchema = z.object({
 		.trim()
 		.refine((val) => typeof val === 'string', { message: '内容必须为字符串' })
 		.min(1, '内容不能为空'),
+	date_path: z
+		.string()
+		.trim()
+		.refine((val) => typeof val === 'string', { message: '日期路径必须为字符串' })
+		.min(1, '日期路径不能为空'),
 	tags: z.array(z.number()).optional().default([]),
 	created_at: z.string().trim().optional(),
 	updated_at: z.string().trim().optional(),
