f8256f871660746001d7a3be29a9b40a6882eba0
huhai <2075313210@qq.com>
2025-11-14 16:10:54 +0800

    修改上传博客文章图片接口

---
+++ server/api/md/mdUpload.post.ts
@@ -0,0 +1,29 @@
+import { container } from '~/server/core/container';
+import { MdService } from '~/server/services/MdService';
+import type { MultiPartData } from 'h3';
+
+export default defineEventHandler(async (event) => {
+	try {
+		const mdService = container.get(MdService);
+		const multipart = await readMultipartFormData(event);
+
+		let datePath = '';
+		const fileList: MultiPartData[] = [];
+
+		multipart?.forEach((item) => {
+			if (item.name === 'datePath') {
+				datePath = item.data.toString();
+			} else if (item.name === 'files') {
+				fileList.push(item);
+			}
+		});
+
+		if (!fileList || fileList.length === 0) {
+			return sendErrorWithMessage(event, 400, '没有上传文件');
+		}
+
+		return await mdService.uploadImage(fileList, datePath);
+	} catch (err) {
+		return sendErrorWithMessage(event, 500, '上传失败');
+	}
+});
