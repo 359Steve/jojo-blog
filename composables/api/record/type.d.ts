7b2022a3d2ce7f01412e81f1b5be395608a071f2
huhai <2075313210@qq.com>
2025-11-16 12:52:40 +0800

    添加类型

--- composables/api/record/type.d.ts
+++ composables/api/record/type.d.ts
@@ -2,10 +2,20 @@ type RecordWithDetailsRep<T, K> = T & {
 	prev: { id: number } | null;
 	next: { id: number } | null;
 	details: Pick<K, 'id' | 'title' | 'image_url' | 'image_alt' | 'time_range'>[];
+	_count: {
+		details: number;
+	};
 };
 
 type RecordDetailRep<T> = T & {
 	imageAll: Pick<T, 'id' | 'image_url' | 'image_alt'>[];
 	prev: { id: number } | null;
 	next: { id: number } | null;
+};
+
+// 分页查询记录参数类型
+type RecordQueryParams = {
+	parentId: number;
+	pageNumber: number;
+	pageSize: number;
 };
