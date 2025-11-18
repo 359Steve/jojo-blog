fed13766b553eee7d12cef2948dfc6cbf4057127
huhai <2075313210@qq.com>
2025-11-18 11:24:28 +0800

    修改类型

--- composables/api/record/type.d.ts
+++ composables/api/record/type.d.ts
@@ -1,14 +1,14 @@
 type RecordWithDetailsRep<T, K> = T & {
 	prev: { id: number } | null;
 	next: { id: number } | null;
-	details: Pick<K, 'id' | 'title' | 'image_url' | 'image_alt' | 'time_range'>[];
+	details: Pick<K, 'id' | 'title' | 'images' | 'image_alt' | 'time_range'>[];
 	_count: {
 		details: number;
 	};
 };
 
 type RecordDetailRep<T> = T & {
-	imageAll: Pick<T, 'id' | 'image_url' | 'image_alt'>[];
+	imageAll: Pick<T, 'id' | 'images' | 'image_alt'>[];
 	prev: { id: number } | null;
 	next: { id: number } | null;
 };
