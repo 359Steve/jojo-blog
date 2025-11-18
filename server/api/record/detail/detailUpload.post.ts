3419a30546e28d3c70a1fd25dd34542803c2dd7a
huhai <2075313210@qq.com>
2025-11-18 11:26:02 +0800

    上传详情图片修改成多张

--- server/api/record/detail/detailUpload.post.ts
+++ server/api/record/detail/detailUpload.post.ts
@@ -1,16 +1,25 @@
 import { container } from '~/server/core/container';
 import { RecordDetailService } from '~/server/services/RecordDetailService';
+import type { MultiPartData } from 'h3';
 
 export default defineEventHandler(async (event) => {
 	try {
 		const recordDetailService = container.get(RecordDetailService);
 		const files = await readMultipartFormData(event);
 
-		if (!files || files.length === 0) {
+		const fileList: MultiPartData[] = [];
+
+		files?.forEach((item) => {
+			if (item.name === 'files') {
+				fileList.push(item);
+			}
+		});
+
+		if (!fileList || fileList.length === 0) {
 			return sendErrorWithMessage(event, 400, '没有上传文件');
 		}
 
-		return await recordDetailService.uploadRecordDetailImage(files);
+		return await recordDetailService.uploadRecordDetailImage(fileList);
 	} catch (err) {
 		return sendErrorWithMessage(event, 500, '上传失败');
 	}
