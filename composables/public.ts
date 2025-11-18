424c83753c32b42ac55e95ee78f55531b68d6edb
huhai <2075313210@qq.com>
2025-11-18 17:00:09 +0800

    创建生成日期标识字符串

--- composables/public.ts
+++ composables/public.ts
@@ -134,3 +134,15 @@ export const chooseCondition = <Req, Rep>(endpoint: string, method: 'GET' | 'POS
 		return handleApiResponse(res, false);
 	};
 };
+
+export const getDatePath = (): string => {
+	const date = new Date();
+	return (
+		`${date.getFullYear()}-${(date.getMonth() + 1)
+			.toString()
+			.padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}` +
+		`-${date.getHours().toString().padStart(2, '0')}` +
+		`${date.getMinutes().toString().padStart(2, '0')}` +
+		`${date.getSeconds().toString().padStart(2, '0')}`
+	);
+};
