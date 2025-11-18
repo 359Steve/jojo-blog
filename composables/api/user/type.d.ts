9ef5a0f8164d122b99153b3eadc9a4a59ddddf71
huhai <2075313210@qq.com>
2025-11-18 11:24:50 +0800

    修改类型

--- composables/api/user/type.d.ts
+++ composables/api/user/type.d.ts
@@ -4,6 +4,6 @@ interface FindPictureRequest {
 	random?: boolean;
 }
 
-interface HomePicResponse<T> extends Pick<T, 'id' | 'image_url' | 'image_alt'> {
-	parent_id: number;
-}
+type HomePicResponse<T> = T & {
+	group_id: number;
+};
