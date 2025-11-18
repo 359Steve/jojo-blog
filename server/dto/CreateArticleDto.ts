16dbf118a8749b828f570a4430611bd20f506641
huhai <2075313210@qq.com>
2025-11-18 11:26:18 +0800

    添加images类型字段

--- server/dto/CreateArticleDto.ts
+++ server/dto/CreateArticleDto.ts
@@ -9,7 +9,7 @@ export const CreateRecordDetailSchema = z.object({
 	title: z.string().min(1, '标题不能为空'),
 	summary: z.string().min(1, '摘要不能为空'),
 	time_range: z.string().min(1, '时间范围不能为空'),
-	image_url: z.string().min(1, '图片URL不能为空'),
+	images: z.array(z.string().min(1, '图片地址不能为空')).optional().default([]),
 	image_alt: z.string().min(1, '图片描述不能为空'),
 	created_at: z.string().trim().optional(),
 	updated_at: z.string().trim().optional(),
