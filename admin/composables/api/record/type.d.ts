b05b0ca313c4e968392f6080d69a7fc3482f6e13
huhai <2075313210@qq.com>
2025-11-18 11:23:20 +0800

    创建详情图片类型

--- admin/composables/api/record/type.d.ts
+++ admin/composables/api/record/type.d.ts
@@ -1,5 +1,12 @@
+interface RecordDetailImages {
+	id: number;
+	url: string;
+	record_detail_id: number;
+}
+
 type GroupWithDetail<T> = T & {
 	group: {
 		time_range: string;
 	};
+	images: RecordDetailImages[];
 };
