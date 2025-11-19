80ffe40071107a90afc4e0ff36dc7c65fd3b0810
huhai <2075313210@qq.com>
2025-11-19 15:05:55 +0800

    修改请求中间件

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -12,39 +12,33 @@ const whitelist = [
 // 白名单路径模式
 const whitelistPatterns = [/^\/api\/blog\/blogPublicDetail(\/\d+)?(\?.*)?$/];
 
-export default defineEventHandler((event) => {
-	let realToken = '';
-	const path = event.node.req.url || '';
+export default defineEventHandler(async (event) => {
+	const path = (event.node.req.url || '').split('?')[0];
 
-	// 去掉携带的参数
-	const realPath = path.split('?')[0];
-
+	// 只处理 /api 开头接口
 	if (!path.startsWith('/api')) return;
 
-	// 如果是白名单接口，跳过校验
-	if (whitelist.includes(realPath)) return;
+	// 检查是否白名单
+	const isWhitelist = whitelist.includes(path) || whitelistPatterns.some((p) => p.test(path));
 
-	// 检查白名单模式
-	if (whitelistPatterns.some((pattern) => pattern.test(path))) return;
+	if (isWhitelist) return;
 
+	let realToken = '';
 	if (import.meta.server) {
 		const token: TokenCookie = parseCookies(event) as any;
 		const cookieToken: { token: string } = JSON.parse((token.userState ?? JSON.stringify({})) as unknown as string);
 		realToken = cookieToken?.token;
 	} else {
-		const authHeader = getHeader(event, 'Authorization');
+		const authHeader = event.node.req.headers['authorization'] as string | undefined;
 		realToken = authHeader?.split(' ')[1] ?? '';
 	}
 
-	if (!realToken) {
-		sendErrorWithMessage(event, 401, '你还未登录！');
-	}
+	if (!realToken) return sendErrorWithMessage(event, 401, '你还未登录！');
 
 	try {
-		// 验证token
-		const payload = verifyToken(realToken || '');
+		const payload = verifyToken(realToken);
 		event.context.user = payload;
 	} catch {
-		sendErrorWithMessage(event, 401, '登录已失效！');
+		return sendErrorWithMessage(event, 401, '登录已失效！');
 	}
 });
