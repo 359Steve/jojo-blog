501ed5b621c101deed4a5c0b7b3d72f7e0b6cb14
huhai <2075313210@qq.com>
2025-11-24 20:57:43 +0800

    限制只能是后台管理页面查询

--- plugins/getUser.ts
+++ plugins/getUser.ts
@@ -3,18 +3,16 @@ export default defineNuxtPlugin({
 	parallel: true,
 	async setup() {
 		const route = useRoute();
-		if (route.path.startsWith('/admin')) {
-			return;
-		}
-
-		const { data } = await useAsyncData('user', () => findPublicUser());
-		if (data.value?.data) {
-			useBlogUserInfo().setBlogUserInfo(data.value.data);
-		} else {
-			throw createError({
-				statusCode: 500,
-				message: '获取用户信息失败！',
-			});
+		if (!route.path.startsWith('/admin')) {
+			const { data } = await useAsyncData('user', () => findPublicUser());
+			if (data.value?.data) {
+				useBlogUserInfo().setBlogUserInfo(data.value.data);
+			} else {
+				throw createError({
+					statusCode: 500,
+					message: '获取用户信息失败！',
+				});
+			}
 		}
 	},
 });
