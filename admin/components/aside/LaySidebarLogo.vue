131f7667fc05219966179abe682b25a4ae046d97
huhai <2075313210@qq.com>
2025-11-24 20:55:48 +0800

    固定后台logo大小

--- admin/components/aside/LaySidebarLogo.vue
+++ admin/components/aside/LaySidebarLogo.vue
@@ -6,7 +6,9 @@ const { Avatar } = getIcons();
 	<div class="relative h-[48px] w-full overflow-hidden">
 		<Transition name="sidebarLogoFade">
 			<RouterLink key="expand" class="flex h-full flex-nowrap items-center pl-[10px]" to="/admin">
-				<Avatar />
+				<div>
+					<Avatar />
+				</div>
 				<span
 					class="m-[2px_0_0_12px] inline-block h-[32px] overflow-hidden text-ellipsis whitespace-nowrap text-[18px] font-[600] leading-[32px] text-admin-sub-menu-active-text">
 					JoJoAdmin
