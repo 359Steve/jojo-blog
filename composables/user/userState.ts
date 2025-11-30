e115454f97b521f3ab11cfa2e4a32a06be3ccf0e
huhai <2075313210@qq.com>
2025-11-30 14:55:00 +0800

    添加是否登录标记

--- composables/user/userState.ts
+++ composables/user/userState.ts
@@ -4,10 +4,16 @@ export const useUserState = defineStore(
 	'userState',
 	() => {
 		const token = ref<string>('');
+		const isUnauthorized = ref<boolean>(false);
 
 		// 获取token
 		const getToken = () => {
 			return token.value;
+		};
+
+		// 获取是否未授权
+		const getIsUnauthorized = () => {
+			return isUnauthorized.value;
 		};
 
 		// 设置token
@@ -15,13 +21,21 @@ export const useUserState = defineStore(
 			token.value = value;
 		};
 
+		// 设置是否未授权
+		const setIsUnauthorized = (value: boolean) => {
+			isUnauthorized.value = value;
+		};
+
 		return {
 			token,
+			isUnauthorized,
+			getIsUnauthorized,
+			setIsUnauthorized,
 			getToken,
-			setToken
+			setToken,
 		};
 	},
 	{
-		persist: true
-	}
+		persist: true,
+	},
 );
