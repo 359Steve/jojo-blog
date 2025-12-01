61f60cff084ed5b6025ad9a9f08329b9f33a90fd
huhai <2075313210@qq.com>
2025-12-01 17:37:02 +0800

    添加预览过度效果

--- components/preview/ImageMask.vue
+++ components/preview/ImageMask.vue
@@ -7,13 +7,33 @@ const isPreviewVisible = defineModel<boolean>('preview');
 </script>
 
 <template>
-	<div v-if="isPreviewVisible" class="fixed bottom-0 left-0 right-0 top-0 z-[500] backdrop-blur-md"
-		@click="isPreviewVisible = false">
-		<div class="h-full w-full">
+	<Transition name="fade" appear>
+		<div v-if="isPreviewVisible" class="fixed bottom-0 left-0 right-0 top-0 z-[500] backdrop-blur-md transition-all"
+			@click="isPreviewVisible = false">
+			<div class="absolute inset-0 z-[-1] bg-black/50" />
 			<img :src="src" :alt="alt"
 				class="z-[2] h-full max-h-[100vh] w-full max-w-[100vw] object-contain sm:aspect-square">
 		</div>
-	</div>
+	</Transition>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+	transition:
+		opacity 0.3s ease,
+		transform 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+	opacity: 0;
+	transform: scale(0.999);
+}
+
+.fade-enter-to,
+.fade-leave-from {
+	opacity: 1;
+	transform: scale(1);
+}
+</style>
