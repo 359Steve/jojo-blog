8a38459bc575b590e872fd92e7c50bbd1bbf04d7
huhai <2075313210@qq.com>
2025-12-02 17:08:51 +0800

    后台首页增加内容

--- admin/pages/admin/index.vue
+++ admin/pages/admin/index.vue
@@ -1,7 +1,45 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+interface CardList {
+	title: string;
+	value: number;
+	component: 'SuggestionList' | 'BlogList' | 'RecordList';
+}
+
+const components = {
+	RecordList: resolveComponent('RecordList'),
+	BlogList: resolveComponent('BlogList'),
+	SuggestionList: resolveComponent('SuggestionList'),
+};
+const cardList = ref<CardList[]>([
+	{ title: '记录数', value: 128, component: 'SuggestionList' },
+	{ title: '博客数', value: 64, component: 'BlogList' },
+	{ title: '建议数', value: 32, component: 'RecordList' },
+]);
+const activeComponentName = ref<CardList['component']>('SuggestionList');
+</script>
 
 <template>
-	<div>后台首页</div>
+	<AdminFormMain title="后台首页">
+		<div class="mb-2 grid w-full grid-cols-1 gap-2 *:gap-4 md:grid-cols-2 xl:grid-cols-3">
+			<ElCard v-for="item in cardList" :key="item.value" shadow="hover"
+				class="!min-h-[128px] !cursor-pointer !rounded-md" @click="activeComponentName = item.component">
+				<div class="flex h-full w-full items-center justify-between gap-1">
+					<Icon name="mdi-chart-box-outline" class="mr-2 inline-block" width="32" />
+					<div class="flex-1">
+						<p class="text-sm text-gray-500">{{ item.title }}</p>
+						<p class="text-2xl font-bold">{{ item.value }}</p>
+					</div>
+					<div class="aspect-square h-full bg-pink-300" />
+				</div>
+			</ElCard>
+		</div>
+
+		<component :is="components[activeComponentName]" />
+	</AdminFormMain>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="postcss" scoped>
+:deep(.el-card__body) {
+	@apply h-full;
+}
+</style>
