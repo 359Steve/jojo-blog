3c8fcf996f856f96e12bbfb5baa62cd8b761478a
huhai <2075313210@qq.com>
2025-12-02 21:10:20 +0800

    完善后台路由拦截器

--- admin/middleware/admin.ts
+++ admin/middleware/admin.ts
@@ -1,30 +1,36 @@
 import type { CookieRef } from 'nuxt/app';
 
-const { getToken } = useUserState();
-
-export default defineNuxtRouteMiddleware(async (to) => {
-	// 路由白名单
+export default defineNuxtRouteMiddleware((to) => {
 	const whiteRoutes = ['/admin/login'];
 	const path = to.path;
-	const userState = useCookie('userState') as CookieRef<{ token: string; isUnauthorized: boolean }>;
-	const user_name = useCookie('user_name');
 
-	if (!whiteRoutes.includes(path) && !path.startsWith('/api')) {
-		if (!getToken()) {
-			return navigateTo('/admin/login');
-		} else {
-			// 验证token
-			if (getToken() !== userState.value?.token || !user_name.value) {
-				ElMessage({
-					type: 'error',
-					message: '登录已失效！',
-				});
-				useUserState().setToken('');
-				useUserState().setIsUnauthorized(false);
-				return navigateTo('/admin/login', { replace: true });
-			}
-		}
+	if (whiteRoutes.includes(path) || path.startsWith('/api')) {
+		return;
 	}
 
-	return true;
+	const userState = useCookie('userState') as CookieRef<{ token: string; isUnauthorized: boolean }>;
+	const cookieToken = userState.value?.token;
+	if (!cookieToken) {
+		return navigateTo('/admin/login', { replace: true });
+	}
+
+	const { getToken } = useUserState();
+	const currentToken = getToken();
+
+	if (currentToken !== cookieToken) {
+		useUserState().setToken('');
+		useUserState().setIsUnauthorized(false);
+
+		if (import.meta.client) {
+			ElMessage({
+				type: 'error',
+				message: '登录已失效！',
+				duration: 2000,
+			});
+		}
+
+		return navigateTo('/admin/login', { replace: true });
+	}
+
+	return;
 });
