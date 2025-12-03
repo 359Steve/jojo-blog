9dd72b56bf43ed5100e7933f95da8eb538d0a00d
huhai <2075313210@qq.com>
2025-12-03 14:41:20 +0800

    添加数据查询展示功能

--- admin/components/home/SuggestionList.vue
+++ admin/components/home/SuggestionList.vue
@@ -1,7 +1,65 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import type { CreateErrorMessageDto } from '~/server/dto/CreateErrorMessageDto';
+
+const pageSize = ref<number>(10);
+const pageNumber = ref<number>(1);
+const { data, refresh } = await useAsyncData(
+	'errorList',
+	() =>
+		fetchErrorList({
+			pageNumber: pageNumber.value,
+			pageSize: pageSize.value,
+		}),
+	{
+		watch: [pageNumber, pageSize],
+	},
+);
+
+const tableData = computed((): CreateErrorMessageDto[] => data.value?.data?.records || []);
+const total = computed((): number => data.value?.data?.total || 0);
+
+// 分页查询
+const handleCurrentChange = (val: number): void => {
+	pageNumber.value = val;
+};
+
+// 每页条数改变
+const handleSizeChange = (val: number): void => {
+	pageSize.value = val;
+	pageNumber.value = 1;
+};
+
+// 删除错误信息
+const deleteErrorClick = async (id: number) => {
+	useConfirm('删除错误信息', 'warning', async () => {
+		const res = await deleteError(id);
+		res.data && refresh();
+	});
+};
+</script>
 
 <template>
-	<div class="w-full">建议列表</div>
+	<TableHeight>
+		<template #default="{ height }">
+			<ElTable :data="tableData" stripe :height="height" class="w-full !text-[16px]">
+				<ElTableColumn fixed prop="id" label="ID" width="80" />
+				<ElTableColumn prop="name" label="名称" width="120" />
+				<ElTableColumn prop="email" label="邮箱" width="280" />
+				<ElTableColumn prop="content" label="内容" show-overflow-tooltip />
+				<ElTableColumn label="操作" width="100">
+					<template #default="scope">
+						<ElButton link type="danger" class="!text-[16px]" size="small"
+							@click="deleteErrorClick(scope.row.id)">
+							删除
+						</ElButton>
+					</template>
+				</ElTableColumn>
+			</ElTable>
+		</template>
+	</TableHeight>
+
+	<AdminFormPagination :total="total" :page-number="pageNumber" :page-size="pageSize"
+		@handle-current-change="handleCurrentChange" @handle-size-change="handleSizeChange" />
 </template>
 
 <style lang="scss" scoped></style>
