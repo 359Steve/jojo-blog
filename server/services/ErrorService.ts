d5e08166ae7097bed956699451c6a3fe56302dc0
huhai <2075313210@qq.com>
2025-12-03 14:44:08 +0800

    创建操作错误信息接口

--- server/services/ErrorService.ts
+++ server/services/ErrorService.ts
@@ -5,4 +5,10 @@ export const ERROR_SERVICE = Symbol.for('ErrorService');
 export interface ErrorService {
 	// 发送邮件
 	sendErrorEmail(errorInfo: CreateErrorMessageDto): ReturnType<ErrorRepository['sendErrorEmail']>;
+
+	// 分页查询错误信息
+	queryErrorList(query: ErrorQueryListParams): ReturnType<ErrorRepository['queryErrorList']>;
+
+	// 删除错误信息
+	deleteError(id: number): ReturnType<ErrorRepository['deleteError']>;
 }
