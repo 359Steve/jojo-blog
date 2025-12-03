cdbd2bd050c7725f19ac44289d9bbc62986fe043
huhai <2075313210@qq.com>
2025-12-03 11:50:51 +0800

    修改实现类和数据访问类的注入方式

--- server/core/container.ts
+++ server/core/container.ts
@@ -1,33 +1,85 @@
 import { Container } from 'inversify';
 import { UserRepository } from '../repositories/UserRepository';
-import { UserService } from '../services/UserService';
+import { USER_SERVICE } from '../services/UserService';
 import { TagRepository } from '../repositories/TagRepository';
-import { TagService } from '../services/TagService';
-import { BlogService } from '../services/BlogService';
+import { TAG_SERVICE } from '../services/TagService';
+import { BLOG_SERVICE } from '../services/BlogService';
 import { BlogRepository } from '../repositories/BlogRepository';
-import { MdService } from '../services/MdService';
+import { MD_SERVICE } from '../services/MdService';
 import { MdRepository } from '../repositories/MdRepository';
-import { GroupService } from '../services/GroupService';
+import { GROUP_SERVICE } from '../services/GroupService';
 import { GroupRepository } from '../repositories/GroupRepository';
-import { RecordDetailService } from '../services/RecordDetailService';
+import { RECORD_DETAIL_SERVICE } from '../services/RecordDetailService';
 import { RecordDetailRepository } from '../repositories/RecordDetailRepository';
-import { ErrorService } from '../services/ErrorService';
+import { ERROR_SERVICE } from '../services/ErrorService';
 import { ErrorRepository } from '../repositories/ErrorRepository';
+import { BlogServiceImpl } from '../serviceImpl/BlogServiceImpl';
+import { UserServiceImpl } from '../serviceImpl/UserServiceImpl';
+import { TagServiceImpl } from '../serviceImpl/TagServiceImpl';
+import { GroupServiceImpl } from '../serviceImpl/GroupServiceImpl';
+import { RecordDetailServiceImpl } from '../serviceImpl/RecordDetailServiceImpl';
+import { MdServiceImpl } from '../serviceImpl/MdServiceImpl';
+import { ErrorServiceImpl } from '../serviceImpl/ErrorServiceImpl';
 
 const container = new Container();
-container.bind(UserRepository).toSelf();
-container.bind(UserService).toSelf();
-container.bind(TagRepository).toSelf();
-container.bind(TagService).toSelf();
-container.bind(BlogRepository).toSelf();
-container.bind(BlogService).toSelf();
-container.bind(MdService).toSelf();
-container.bind(MdRepository).toSelf();
-container.bind(GroupService).toSelf();
-container.bind(GroupRepository).toSelf();
-container.bind(RecordDetailService).toSelf();
-container.bind(RecordDetailRepository).toSelf();
-container.bind(ErrorService).toSelf();
-container.bind(ErrorRepository).toSelf();
+
+// 绑定 Repositories
+container.bind(UserRepository).toSelf().inSingletonScope();
+container.bind(TagRepository).toSelf().inSingletonScope();
+container.bind(BlogRepository).toSelf().inSingletonScope();
+container.bind(MdRepository).toSelf().inSingletonScope();
+container.bind(GroupRepository).toSelf().inSingletonScope();
+container.bind(RecordDetailRepository).toSelf().inSingletonScope();
+container.bind(ErrorRepository).toSelf().inSingletonScope();
+
+// 绑定 Services 并手动注入依赖
+container
+	.bind(USER_SERVICE)
+	.toDynamicValue(() => {
+		return new UserServiceImpl(container.get(UserRepository));
+	})
+	.inSingletonScope();
+
+container
+	.bind(TAG_SERVICE)
+	.toDynamicValue(() => {
+		return new TagServiceImpl(container.get(TagRepository));
+	})
+	.inSingletonScope();
+
+container
+	.bind(BLOG_SERVICE)
+	.toDynamicValue(() => {
+		return new BlogServiceImpl(container.get(BlogRepository));
+	})
+	.inSingletonScope();
+
+container
+	.bind(MD_SERVICE)
+	.toDynamicValue(() => {
+		return new MdServiceImpl(container.get(MdRepository));
+	})
+	.inSingletonScope();
+
+container
+	.bind(GROUP_SERVICE)
+	.toDynamicValue(() => {
+		return new GroupServiceImpl(container.get(GroupRepository));
+	})
+	.inSingletonScope();
+
+container
+	.bind(RECORD_DETAIL_SERVICE)
+	.toDynamicValue(() => {
+		return new RecordDetailServiceImpl(container.get(RecordDetailRepository));
+	})
+	.inSingletonScope();
+
+container
+	.bind(ERROR_SERVICE)
+	.toDynamicValue(() => {
+		return new ErrorServiceImpl(container.get(ErrorRepository));
+	})
+	.inSingletonScope();
 
 export { container };
