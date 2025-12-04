a81e4b29eff70449dd2e2fdab7455ef868d6c5d4
huhai <2075313210@qq.com>
2025-12-04 18:58:09 +0800

    封装增加博客浏览量方法

--- composables/api/blog/index.ts
+++ composables/api/blog/index.ts
@@ -10,3 +10,6 @@ export const getPublicBlogList = chooseCondition<
 // 查询博客详情
 export const getPublicBlogDetail = (id: number) =>
 	chooseCondition<any, BlogWithTagsRep<CreateBlogDto, CreateTagDto, 'tags'>>(`/blog/blogPublicDetail/${id}`)();
+
+// 增加博客浏览量
+export const addBlogView = (id: number) => chooseCondition<any, CreateBlogDto>(`/blog/blogAddView/${id}`, 'PUT')();
