1ac27afdc8275b0b335da76376335c55ce8b681d
huhai <2075313210@qq.com>
2025-12-05 14:37:14 +0800

    添加注释

--- admin/composables/api/statistical/index.ts
+++ admin/composables/api/statistical/index.ts
@@ -1,5 +1,6 @@
 import type { StatisticalDto } from '~/server/dto/StatisticalDto';
 
+// 获取全部统计数据
 export const getStatisticalAll = async () => {
 	const res = await jojoLoadingIndicator(() =>
 		fetchUseGet<any, Record<string, StatisticalDto>>('/statistical/statisticalAll'),
