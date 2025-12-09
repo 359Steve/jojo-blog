7f44cbe8f35db17b1fc62d4ac2b81533ff2b7ce1
huhai <2075313210@qq.com>
2025-12-09 10:38:12 +0800

    删除查询记录详情列表接口请求参数

--- server/api/record/recordPublicDetails.get.ts
+++ server/api/record/recordPublicDetails.get.ts
@@ -3,11 +3,9 @@ import type { RecordDetailService } from '~/server/services/RecordDetailService'
 import { RECORD_DETAIL_SERVICE } from '~/server/services/RecordDetailService';
 
 export default defineEventHandler(async (event) => {
-	const query = getQuery<RecordQueryParams>(event);
-
 	try {
 		const recordDetailService = container.get<RecordDetailService>(RECORD_DETAIL_SERVICE);
-		return await recordDetailService.getPublicRecordDetails(query);
+		return await recordDetailService.getPublicRecordDetails();
 	} catch {
 		sendErrorWithMessage(event, 500, '记录详情查询失败');
 		return null;
