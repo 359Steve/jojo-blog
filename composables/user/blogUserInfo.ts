21c42dba195b1cbc9efbf895f5bf9dfde145d1e5
huhai <2075313210@qq.com>
2025-12-15 11:11:56 +0800

    设置用户基本信息持久化

--- composables/user/blogUserInfo.ts
+++ composables/user/blogUserInfo.ts
@@ -1,20 +1,26 @@
 import type { CreateTagDto } from '~/server/dto/CreateTagDto';
 import type { CreateUserDto } from '~/server/dto/CreateUserDto';
 
-export const useBlogUserInfo = defineStore('blogUserInfo', () => {
-	const userInfo = ref<UserWithTagsRep<Omit<CreateUserDto, 'password'>, CreateTagDto, 'tags'> | null>(null);
+export const useBlogUserInfo = defineStore(
+	'blogUserInfo',
+	() => {
+		const userInfo = ref<UserWithTagsRep<Omit<CreateUserDto, 'password'>, CreateTagDto, 'tags'> | null>(null);
 
-	const getBlogUserInfo = () => {
-		return userInfo.value;
-	};
+		const getBlogUserInfo = () => {
+			return userInfo.value;
+		};
 
-	const setBlogUserInfo = (info: UserWithTagsRep<Omit<CreateUserDto, 'password'>, CreateTagDto, 'tags'>) => {
-		userInfo.value = info;
-	};
+		const setBlogUserInfo = (info: UserWithTagsRep<Omit<CreateUserDto, 'password'>, CreateTagDto, 'tags'>) => {
+			userInfo.value = info;
+		};
 
-	return {
-		userInfo,
-		getBlogUserInfo,
-		setBlogUserInfo,
-	};
-});
+		return {
+			userInfo,
+			getBlogUserInfo,
+			setBlogUserInfo,
+		};
+	},
+	{
+		persist: true,
+	},
+);
