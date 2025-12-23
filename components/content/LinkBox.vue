234acebf580fa689345aec3686ef9e3706ee90e0
huhai <2075313210@qq.com>
2025-12-23 09:45:51 +0800

    创建测试组件

---
+++ components/content/LinkBox.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const { title } = defineProps<{
+	title: string;
+}>();
+</script>
+
+<template>
+	<div class="w-full">
+		<h3 :id="title" class="mb-2 font-[600] text-inherit">
+			{{ title }}
+			<a :href="`#${title}`" aria-hidden="true" />
+		</h3>
+		<div>
+			<span>
+				<slot mdc-unwrap="p" />
+			</span>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
