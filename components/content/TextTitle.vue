5a5782cafeeccaf20de18deabb6af9a195153b9f
huhai <2075313210@qq.com>
2025-12-23 09:46:07 +0800

    创建测试组件

---
+++ components/content/TextTitle.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+	<div class="w-full">
+		<h1 class="font-[600]">
+			<slot mdc-unwrap="p" />
+		</h1>
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
