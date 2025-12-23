c404e29e889a2cb99bbd6bfdf045631c636f0e72
huhai <2075313210@qq.com>
2025-12-23 09:46:40 +0800

    添加md文档错误判断

--- pages/content/blog/[...name].vue
+++ pages/content/blog/[...name].vue
@@ -1,10 +1,30 @@
 <script lang="ts" setup>
 const route = useRoute();
-
-const { data: page } = await useAsyncData(route.path, () => {
-	return queryCollection('content')
+const { data: page, error } = await useAsyncData(route.path, async () => {
+	const data = await queryCollection('content')
 		.path(route.path.replace(/^\/content/, ''))
 		.first();
+
+	if (!data) {
+		throw createError({
+			statusCode: 404,
+			message: '博客不存在',
+		});
+	}
+
+	return data;
+});
+
+if (error.value) {
+	throw createError({
+		statusCode: error.value.statusCode || 404,
+		message: error.value.message || '获取博客失败',
+	});
+}
+
+useSeoMeta({
+	title: page.value?.title || '博客页面',
+	description: page.value?.description || '博客页面描述',
 });
 </script>
 
