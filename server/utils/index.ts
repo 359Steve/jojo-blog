203ee7fc75ac68f988e0bff45608a871e6c2206f
huhai <2075313210@qq.com>
2025-12-23 15:11:04 +0800

    增加文件存储路径方法，将文件存储在打包后的public里

--- server/utils/index.ts
+++ server/utils/index.ts
@@ -1,4 +1,21 @@
 import type { ZodObject } from 'zod';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import fs from 'node:fs';
+
+const findOutputPublicDir = (startDir: string): string => {
+	let dir = startDir;
+
+	while (dir !== '/') {
+		const candidate = join(dir, '.output/public');
+		if (fs.existsSync(candidate)) {
+			return candidate;
+		}
+		dir = dirname(dir);
+	}
+
+	throw new Error('Cannot find .output/public directory');
+};
 
 export const validateData = <T>(schema: ZodObject, data: T, onError: (value: string) => void): T => {
 	const result = schema.safeParse(data);
@@ -13,4 +30,18 @@ export const validateData = <T>(schema: ZodObject, data: T, onError: (value: str
 	}
 
 	return result.data as T; // 校验通过的数据
+};
+
+export const getPublicDir = (): string => {
+	// 当前执行文件所在目录
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = dirname(__filename);
+
+	// 生产环境
+	if (process.env.NODE_ENV === 'production') {
+		return findOutputPublicDir(__dirname);
+	}
+
+	// 开发环境
+	return join(process.cwd(), 'public');
 };
