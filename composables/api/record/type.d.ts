3d04f17faafef0d1813ba4e7aa10fcd04c43324b
huhai <2075313210@qq.com>
2025-12-26 18:36:07 +0800

    修改图片类型增加blurhash字段

--- composables/api/record/type.d.ts
+++ composables/api/record/type.d.ts
@@ -1,14 +1,26 @@
+type Details<T, K extends keyof T> = Pick<T, K> & {
+	images: {
+		url: string;
+		blurhash: string;
+	}[];
+};
+
 type RecordWithDetailsRep<T, K> = T & {
 	prev: { id: number } | null;
 	next: { id: number } | null;
-	details: Pick<K, 'id' | 'title' | 'images' | 'image_alt' | 'time_range' | 'views'>[];
+	details: Details<K, 'id' | 'title' | 'image_alt' | 'time_range' | 'views'>[];
 	_count: {
 		details: number;
 	};
 };
 
-type RecordDetailRep<T> = T & {
-	imageAll: Pick<T, 'id' | 'images' | 'image_alt'>[];
+type RecordDetailRep<T> = Omit<T, 'images'> & {
+	images: {
+		url: string;
+		blurhash: string;
+	}[];
+} & {
+	imageAll: Details<T, 'id' | 'images' | 'image_alt'>[];
 	prev: { id: number } | null;
 	next: { id: number } | null;
 };
