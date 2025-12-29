07c387e516b929250a1b7d44cb149a4ff8b54083
huhai <2075313210@qq.com>
2025-12-29 17:34:31 +0800

    创建md博客详情页面

---
+++ pages/content/blog/[name].vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+const route = useRoute();
+const name = computed(() => (route.params as { name: string }).name);
+
+const { data: page, error } = await useAsyncData(route.path, async () => {
+	const data = await queryCollection('blog')
+		.path(`/blog/${name.value}`)
+		.first();
+
+	if (!data) {
+		throw createError({
+			statusCode: 404,
+			message: '博客不存在',
+		});
+	}
+
+	return data;
+});
+
+if (error.value) {
+	throw createError({
+		statusCode: error.value.statusCode || 404,
+		message: error.value.message || '获取博客失败',
+	});
+}
+
+useSeoMeta({
+	title: page.value?.title || '博客页面',
+	description: page.value?.description || '博客页面描述',
+});
+</script>
+
+<template>
+	<div class="w-full">
+		<ContentRenderer v-if="page" :value="page" />
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
