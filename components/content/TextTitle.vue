ba37216b71a9f06eaed35732f26e1966395ae36f
huhai <2075313210@qq.com>
2025-12-30 18:15:39 +0800

    md文件的title组件修改样式

--- components/content/TextTitle.vue
+++ components/content/TextTitle.vue
@@ -1,11 +1,36 @@
-<script lang="ts" setup></script>
+<script setup lang="ts"></script>
 
 <template>
 	<div class="w-full">
-		<h1 class="font-[600]">
-			<slot mdc-unwrap="p" />
-		</h1>
+		<slot />
 	</div>
 </template>
 
-<style lang="scss" scoped></style>
+<style scoped lang="postcss">
+:deep(h2),
+:deep(h3),
+:deep(h4),
+:deep(h5),
+:deep(h6) {
+	@apply relative text-xl font-[600] my-3 leading-[1.6];
+}
+
+:deep(h2 > a),
+:deep(h3 > a),
+:deep(h4 > a) {
+	@apply no-underline text-inherit;
+}
+
+:deep(h2 > a::before),
+:deep(h3 > a::before),
+:deep(h4 > a::before) {
+	content: "#";
+	@apply absolute left-0 -ml-5 opacity-0 transition-opacity font-normal text-gray-400;
+}
+
+:deep(h2:hover > a::before),
+:deep(h3:hover > a::before),
+:deep(h4:hover > a::before) {
+	@apply opacity-100
+}
+</style>
