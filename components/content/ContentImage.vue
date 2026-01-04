c0294404feb7ee55cc6ed5554840aa30369799c1
huhai <2075313210@qq.com>
2026-01-04 21:25:59 +0800

    设置单图时显示方式

--- components/content/ContentImage.vue
+++ components/content/ContentImage.vue
@@ -19,12 +19,18 @@ const preview = (src: string) => {
 		<div class="w-full">
 			<slot mdc-unwrap="p" />
 		</div>
-		<div
+
+		<!-- 单图 -->
+		<div v-if="images.length === 1" class="mt-4 w-full">
+			<img :src="images[0]" alt="Image" class="w-full cursor-pointer aspect-video object-cover rounded"
+				@click="preview(images[0])">
+		</div>
+
+		<!-- 多图 -->
+		<div v-else
 			class="left-0 mt-4 flex h-[200px] sm:h-[260px] md:h-[320px] lg:h-[380px] max-h-[400px] gap-4 overflow-x-auto">
-			<img v-for="(image, index) in images" :key="index" class="aspect-square h-full object-cover" :src="image"
-				:alt="`Image ${index + 1}`">
+			<img v-for="(image, index) in images" :key="index" :src="image" :alt="`Image ${index + 1}`"
+				class="aspect-square cursor-pointer h-full object-cover rounded" @click="preview(image)">
 		</div>
 	</div>
 </template>
-
-<style lang="scss" scoped></style>
