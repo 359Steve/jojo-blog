ab5f3f5cb76fabc4964f10197a9a853293bb5ab2
huhai <2075313210@qq.com>
2026-01-05 17:25:49 +0800

    修改图片和描述获取方式

--- components/preview/ImageMask.vue
+++ components/preview/ImageMask.vue
@@ -1,17 +1,13 @@
 <script lang="ts" setup>
-const { src = '', alt = '' } = defineProps<{
-	src: string;
-	alt: string;
-}>();
-const isPreviewVisible = defineModel<boolean>('preview');
+const { getPreviewSrc, getPreviewVisible, getAlt, setPreviewVisible } = usePreviewImg();
 </script>
 
 <template>
 	<Transition name="fade" appear>
-		<div v-if="isPreviewVisible" class="glass fixed bottom-0 left-0 right-0 top-0 z-[500] transition-all"
-			@click="isPreviewVisible = false">
+		<div v-if="getPreviewVisible()" class="glass fixed bottom-0 left-0 right-0 top-0 z-[500] transition-all"
+			@click="setPreviewVisible(false)">
 			<div class="absolute inset-0 z-[-1] bg-black/50" />
-			<img :src="src" :alt="alt" loading="lazy" decoding="async"
+			<img :src="getPreviewSrc()" :alt="getAlt()" loading="lazy" decoding="async"
 				class="z-[2] h-full max-h-[100vh] w-full max-w-[100vw] object-contain sm:aspect-square">
 		</div>
 	</Transition>
