828cf2d5d50a060e3eb484ae781b7e42d2ce1c08
huhai <2075313210@qq.com>
2026-01-05 16:02:06 +0800

    创建ProseImg组件

---
+++ components/content/ProseImg.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { src, alt, title } = defineProps<{
+	src?: string
+	alt?: string
+	title?: string
+}>()
+</script>
+
+<template>
+	<figure class="flex flex-col items-center">
+		<img v-bind="$attrs" :src="src" :alt="alt" :title="title" loading="lazy" decoding="async" class="
+				w-full
+				rounded
+				aspect-video
+				object-cover
+				cursor-pointer
+			">
+	</figure>
+</template>
