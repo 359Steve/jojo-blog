299cc659b8bbaa541307febfef6afb5590dbef15
huhai <2075313210@qq.com>
2026-01-05 16:01:44 +0800

    还原ContentImage组件代码

--- components/content/ContentImage.vue
+++ components/content/ContentImage.vue
@@ -2,35 +2,19 @@
 const { images } = defineProps<{
 	images: string[];
 }>();
-
-const previewSrc = ref<string>('')
-const isPreviewVisible = ref<boolean>(false);
-
-const preview = (src: string) => {
-	previewSrc.value = src;
-	isPreviewVisible.value = true;
-};
 </script>
 
 <template>
-	<PreviewImageMask v-model:preview="isPreviewVisible" :src="previewSrc" alt="预览照片"
-		@click="isPreviewVisible = false" />
 	<div class="w-full">
 		<div class="w-full">
 			<slot mdc-unwrap="p" />
 		</div>
-
-		<!-- 单图 -->
-		<div v-if="images.length === 1" class="mt-4 w-full">
-			<img :src="images[0]" alt="Image" class="w-full cursor-pointer aspect-video object-cover rounded"
-				@click="preview(images[0])">
-		</div>
-
-		<!-- 多图 -->
-		<div v-else
+		<div
 			class="left-0 mt-4 flex h-[200px] sm:h-[260px] md:h-[320px] lg:h-[380px] max-h-[400px] gap-4 overflow-x-auto">
-			<img v-for="(image, index) in images" :key="index" :src="image" :alt="`Image ${index + 1}`"
-				class="aspect-square cursor-pointer h-full object-cover rounded" @click="preview(image)">
+			<img v-for="(image, index) in images" :key="index" class="aspect-video h-full object-cover" :src="image"
+				:alt="`Image ${index + 1}`">
 		</div>
 	</div>
 </template>
+
+<style lang="scss" scoped></style>
