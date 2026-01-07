d9fe68db1d29a4519790d76e9a4ea3dc09a0f584
huhai <2075313210@qq.com>
2026-01-07 17:48:51 +0800

    完善多图横向滚动逻辑

--- components/content/ContentImage.vue
+++ components/content/ContentImage.vue
@@ -21,17 +21,12 @@ onMounted(() => {
 </script>
 
 <template>
-	<div class="w-full">
-		<div class="w-full">
-			<slot mdc-unwrap="p" />
-		</div>
-		<div
-			class="left-0 mt-4 flex h-[200px] sm:h-[260px] md:h-[320px] lg:h-[380px] max-h-[400px] gap-4 overflow-x-auto">
-			<img v-for="(src, index) in images" :key="index" v-preview-img="{
-				src,
-				alt: `Image ${index + 1}`
-			}" class="aspect-video h-full object-cover" :src="src" :alt="`Image ${index + 1}`">
-		</div>
+	<div ref="containerRef"
+		class="-mx-[var(--prose-margin)] px-[var(--prose-margin)] mt-4 flex py-3 box-border h-[400px] gap-4 overflow-x-auto">
+		<img v-for="(src, index) in images" :key="index" v-preview-img="{
+			src,
+			alt: `Image ${index + 1}`
+		}" class="aspect-square h-full object-cover cursor-pointer" :src="src" :alt="`Image ${index + 1}`">
 	</div>
 </template>
 
