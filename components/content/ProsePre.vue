69539e1fbe4eed50c9178763ddc50273eb12351a
huhai <2075313210@qq.com>
2026-01-07 17:49:00 +0800

    添加折叠功能

--- components/content/ProsePre.vue
+++ components/content/ProsePre.vue
@@ -7,6 +7,14 @@ const props = defineProps<{
 	highlights?: number[]
 }>()
 
+const meta = computed(() => {
+	return props.meta?.split(';')
+})
+
+const fold = ref<boolean>(meta.value?.[1] === 'true')
+
+const icon = computed(() => meta.value?.[0] ?? '')
+
 const { copy } = useClipboard()
 const isCopy = ref<boolean>(false)
 
@@ -18,21 +26,22 @@ const copyed = async () => {
 
 <template>
 	<div class="my-5 rounded-md border dark:border-[#314158]">
-		<div class="group rounded-t-md relative flex items-center justify-between bg-white dark:bg-[#020618] gap-1.5 border-b dark:border-[#314158] px-4 py-2"
+		<div class="group cursor-pointer relative flex items-center justify-between bg-white dark:bg-[#020618] gap-1.5 px-4 py-2"
+			:class="[fold ? 'rounded-md' : 'rounded-t-md border-b dark:border-[#314158]']" @click="fold = !fold"
 			@mouseleave="isCopy = false">
 			<div class="flex items-center gap-2">
-				<Icon :icon="props.meta" class="size-4 shrink-0" aria-hidden="true" />
+				<Icon :icon="icon" class="size-4 shrink-0" aria-hidden="true" />
 				<span class="text-default text-sm/6">{{ props.language }}</span>
 			</div>
 			<div class="absolute items-center gap-2 right-4 md:hidden md:group-hover:flex">
 				<span v-if="isCopy" class="text-[14px]">复制成功</span>
-				<div class="cursor-pointer p-1 border rounded-md dark:border-[#314158]" @click="copyed">
+				<div class="cursor-pointer p-1 border rounded-md dark:border-[#314158]" @click.stop="copyed">
 					<Icon icon="mdi:content-copy" />
 				</div>
 			</div>
 		</div>
 		<pre class="group rounded-b-md font-mono text-sm/6 overflow-x-auto px-4 py-3 bg-[#F8FAFC] dark:bg-[#0F172C]"
-			v-bind="$attrs"><code><slot/></code></pre>
+			:class="[fold ? 'hidden' : 'block']" v-bind="$attrs"><code><slot/></code></pre>
 	</div>
 </template>
 
