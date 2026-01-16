b81f5dfebae6c4a758658ea402a4d5b7bfb0a689
huhai <2075313210@qq.com>
2026-01-16 15:12:46 +0800

    创建ProseOl组件

---
+++ components/content/ProseOl.vue
@@ -0,0 +1,15 @@
+<script lang='ts' setup>
+
+</script>
+
+<template>
+	<ol v-bind="$attrs" class="my-4 pl-6 space-y-2">
+		<slot mdc-unwrap="p" />
+	</ol>
+</template>
+
+<style lang='postcss' scoped>
+:deep(.prose-li) {
+	@apply list-decimal marker:text-black dark:marker:text-white
+}
+</style>
