6550b1fa66406b50f8c716a6f42ea6fc56721f44
huhai <2075313210@qq.com>
2026-01-16 15:12:20 +0800

    常见ProseCode组件

---
+++ components/content/ProseCode.vue
@@ -0,0 +1,12 @@
+<script lang='ts' setup>
+
+</script>
+
+<template>
+	<code v-bind="$attrs"
+		class="px-1.5 py-0.5 text-sm font-mono font-medium rounded-md inline-block border border-blog-ui-border-muted bg-blog-ui-bg-muted">
+		<slot mdc-unwrap="p"/>
+	</code>
+</template>
+
+<style lang='scss' scoped></style>
