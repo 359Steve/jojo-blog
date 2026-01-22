28d7a48a5f258d844288318559894a74416423a8
huhai <2075313210@qq.com>
2026-01-22 18:24:20 +0800

    创建实况图片地址状态管理

--- composables/previewImg.ts
+++ composables/previewImg.ts
@@ -2,6 +2,7 @@ export const usePreviewImg = defineStore('previewImg', () => {
 	const previewVisible = ref<boolean>(false);
 	const previewSrc = ref<string>('');
 	const alt = ref<string>('');
+	const is_live = ref<string>('');
 
 	const setPreviewVisible = (value: boolean) => {
 		previewVisible.value = value;
@@ -13,6 +14,10 @@ export const usePreviewImg = defineStore('previewImg', () => {
 
 	const setAlt = (value: string) => {
 		alt.value = value;
+	};
+
+	const setIsLive = (value: string) => {
+		is_live.value = value;
 	};
 
 	const getAlt = () => {
@@ -27,15 +32,22 @@ export const usePreviewImg = defineStore('previewImg', () => {
 		return previewSrc.value;
 	};
 
+	const getIsLive = () => {
+		return is_live.value;
+	};
+
 	return {
 		previewVisible,
 		previewSrc,
 		alt,
+		is_live,
+		getIsLive,
 		getPreviewSrc,
 		getPreviewVisible,
 		getAlt,
 		setAlt,
 		setPreviewSrc,
 		setPreviewVisible,
+		setIsLive,
 	};
 });
